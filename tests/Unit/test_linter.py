280318bd1a22fe774d0644934e1dc5d1ca8e071e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-11 16:30:03 -0500

    Update test_linter.py

--- tests/Unit/test_linter.py
+++ tests/Unit/test_linter.py
@@ -3,20 +3,20 @@ from vba_linter.linter import Linter
 
 
 line_ending_data = [
-    ('\r\n', ""),
+    ('\r\n', []),
     (
         'Public Function Foo(num)\r\nEnd Function\n',
-        "line: 2 incorrect line ending\n"
+        [("W200", 2)]
     ),
     (
         'Public Function Foo(num)\nEnd Function\n',
-        "line: 1 incorrect line ending\nline: 2 incorrect line ending\n"
+        [("W200", 1), ("W200", 2)]
     ),
 ]
 
 
 @pytest.mark.parametrize("code, expected", line_ending_data)
-def test_line_ending(code: str, expected: str) -> None:
+def test_line_ending(code: str, expected: list) -> None:
     linter = Linter()
     assert linter.lint(code) == expected
 
@@ -24,12 +24,12 @@ def test_line_ending(code: str, expected: str) -> None:
 eol_ws_data = [
     (
         'Public Function Foo(num) \r\nEnd Function\r\n',
-        "line: 1 trailing whitespace\n"
+        [("W400", 1)]
     ),
 ]
 
 
 @pytest.mark.parametrize("code, expected", eol_ws_data)
-def test_eol_ws(code: str, expected: str) -> None:
+def test_eol_ws(code: str, expected: list) -> None:
     linter = Linter()
     assert linter.lint(code) == expected
