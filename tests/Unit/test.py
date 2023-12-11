fb55a9dc9782382767ed4b4727cbb712abf036fa
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-11 12:05:31 -0500

    Update test.py

--- tests/Unit/test.py
+++ tests/Unit/test.py
@@ -1,1 +1,8 @@
+from __main__ import main
 
+
+def test_line_ending() -> None:
+    assert main("\n") == "fail"
+    assert main("\r\n") == ""
+    assert main("\r") == "fail"
+    assert main("\r \n") == "fail"
