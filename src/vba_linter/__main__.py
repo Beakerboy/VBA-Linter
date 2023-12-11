11ce5fca1eed3a451318e89aec17b537c5c29b52
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-11 12:40:33 -0500

    Update __main__.py

--- src/vba_linter/__main__.py
+++ src/vba_linter/__main__.py
@@ -10,20 +10,11 @@ def main(argv: list) -> str:
     tokens = lexer.getAllTokens()
     for token in tokens:
         output = ""
-        if token.type == vbaLexer.WS:
-            cr = False
-            lf = False
-            if "\n" in token.text:
-                lf = True
-            if "\r" in token.text:
-                cr = True
-            if cr and lf:
-                if token.text.index("\r") + 1 != token.text.index("\n"):
-                    return "line: " + "incorrect line ending"
-            if cr != lf:
+        if token.type == vbaLexer.NEWLINE:
+            if token.text == "\n":
                 return "line: " + "incorrect line ending"
         output += str((token.text, token.type))
-        return output
+    return output
 
 if __name__ == '__main__':
     main(sys.argv)
