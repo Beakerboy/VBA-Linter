224da0851fc74af09bb6176819fb292195d5b68a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-11 16:33:30 -0500

    Update test_linter.py

--- tests/Unit/test_linter.py
+++ tests/Unit/test_linter.py
@@ -6,11 +6,11 @@ line_ending_data = [
     ('\r\n', []),
     (
         'Public Function Foo(num)\r\nEnd Function\n',
-        [("W200", 2)]
+        [("W400", 2)]
     ),
     (
         'Public Function Foo(num)\nEnd Function\n',
-        [("W200", 1), ("W200", 2)]
+        [("W400", 1), ("W200", 2)]
     ),
 ]
 
@@ -24,7 +24,7 @@ def test_line_ending(code: str, expected: list) -> None:
 eol_ws_data = [
     (
         'Public Function Foo(num) \r\nEnd Function\r\n',
-        [("W400", 1)]
+        [("W200", 1)]
     ),
 ]
 
