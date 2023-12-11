f65dc9697d2c21f27774db8890cc85ac7bc97ed4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-11 17:17:14 -0500

    Create vbaLexer.pyi

---
+++ src/antlr/vbaLexer.pyi
@@ -0,0 +1,10 @@
+from antlr4 import *
+from _typeshed import Incomplete
+from io import StringIO as StringIO
+from typing import TextIO
+
+def serializedATN(): ...
+
+class vbaLexer(Lexer):
+    NEWLINE: int
+    WS: int
