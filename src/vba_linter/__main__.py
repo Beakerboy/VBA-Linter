c38487db6e3b5f96a1736f070acb85301218819f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-11 13:41:15 -0500

    Update __main__.py

--- src/vba_linter/__main__.py
+++ src/vba_linter/__main__.py
@@ -15,7 +15,7 @@ def main(argv: list) -> str:
             if token.text == "\n":
                 output += "line: " + str(line_num) + " incorrect line ending\n"
             else:
-                output += str(token.type)
+                output += str(prev_tok.type)
             if prev_tok == vbaLexer.WS:
                 output += "line: " + str(line_num)
                 output += " whitespace at the end of the line.\n"
