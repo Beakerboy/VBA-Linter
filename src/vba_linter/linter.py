f6138293e394c9170c21cc079c4e2f6a56856b3b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-12 10:16:24 -0500

    Update linter.py

--- src/vba_linter/linter.py
+++ src/vba_linter/linter.py
@@ -17,7 +17,7 @@ class Linter:
         tokens = lexer.getAllTokens()
         line_num = 1
         output = []
-        prev_tok = None  # type: antlr4.Token
+        prev_tok = None  # type: Token
         for token in tokens:
             if token.type == vbaLexer.NEWLINE:
                 if not (prev_tok is None) and prev_tok.type == vbaLexer.WS:
