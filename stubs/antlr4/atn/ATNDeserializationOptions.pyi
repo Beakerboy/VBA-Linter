e6be55bca6bbfc5105ecf90ad0da808a548d2682
Beakerboy <Beakerboy@1700098.no-reply.drupal.org>
2023-12-12 14:13:08 +0000

    added stubs

---
+++ stubs/antlr4/atn/ATNDeserializationOptions.pyi
@@ -0,0 +1,11 @@
+from _typeshed import Incomplete
+
+ATNDeserializationOptions: Incomplete
+
+class ATNDeserializationOptions:
+    defaultOptions: Incomplete
+    readonly: bool
+    verifyATN: Incomplete
+    generateRuleBypassTransitions: Incomplete
+    def __init__(self, copyFrom: ATNDeserializationOptions = ...) -> None: ...
+    def __setattr__(self, key, value) -> None: ...
