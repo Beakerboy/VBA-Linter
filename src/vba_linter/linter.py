84e961183fa5f02909f51b4f93be411bcaed4f9c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-12 16:25:37 -0500

    Update linter.py

--- src/vba_linter/linter.py
+++ src/vba_linter/linter.py
@@ -35,6 +35,8 @@ class Linter:
                         line_num += 1
                         i += 1
             prev_tok = token
+        if pref_tok.type != vbaLexer.NEWLINE:
+            output.append((line_num - 1, "W201"))
         output.sort()
         return output
 
