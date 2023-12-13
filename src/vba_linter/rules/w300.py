661e13cd57a7d9f0955436b05dc7db24be55dc55
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 16:29:59 -0500

    Create w300.py

---
+++ src/vba_linter/rules/w300.py
@@ -0,0 +1,22 @@
+from antlr.vbaLexer import vbaLexer
+from vba_linter.rules.rule_base import RuleBase
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='W300')
+
+
+class W300(RuleBase):
+    def __init__(self: T) -> None:
+        self.rule_name = "W300"
+
+    def test(self: T, tokens: list) -> list:
+        output: list[tuple] = []
+        final_token = tokens[-1]
+        if final_token.type == vbaLexer.NEWLINE
+            newline_list = RuleBase.split_nl(final_token.text)
+            num_nl = len(newline_list)
+            if num_nl > 1:
+                for i in range(num_nl - 1):
+                    output.append((final_token.line + i + 1, "W300"))
+        return output
