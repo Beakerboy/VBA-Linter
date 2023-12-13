ed9c9bacbb2944cfb097724042169f52e99bc377
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 10:12:19 -0500

    Remove manual line and character counts

--- src/vba_linter/linter.py
+++ src/vba_linter/linter.py
@@ -18,37 +18,32 @@ class Linter:
         input_stream = InputStream(code)
         lexer = vbaLexer(input_stream)
         tokens = lexer.getAllTokens()
-        line_num = 1
         output: list[tuple] = []
         prev_tok = None
-        line_chars = 0
         for token in tokens:
             if token.type == vbaLexer.NEWLINE:
-                if line_chars > max_len:
-                    output.append((line_num, "W501", line_chars))
+                if token.column > max_len:
+                    output.append((token.line, "W501", token.column))
                 if not (prev_tok is None) and prev_tok.type == vbaLexer.WS:
-                    output.append((line_num, "W200"))
-                num = len(token.text)
-                i = 0
-                while i < num:
-                    if num >= 2 and token.text[i:i+2] == '\r\n':
-                        i += 2
-                    else:
-                        output.append((line_num, "W500"))
-                        i += 1
-                    line_num += 1
-                    line_chars = 0
-            else:
-                line_chars += len(token.text)
+                    output.append((token.line, "W200"))
+                newline_list = Linter.split_nl(token.text)
+                num_nl = len(newline_list)
+                for i in range(num_nl):
+                    if newline_list[i] != '\r\n':
+                        output.append((token.line + i, "W500"))
             prev_tok = token
-        if prev_tok is None or prev_tok.type != vbaLexer.NEWLINE:
-            output.append((line_num, "W201"))
-        elif prev_tok.type == vbaLexer.NEWLINE:
-            newline_list = Linter.split_nl(prev_tok.text)
+
+        # End of file checks
+        final_token = prev_tok
+        if final_token is None or final_token.type != vbaLexer.NEWLINE:
+            line = 1 if final_token is None else final_token.line
+            output.append((line, "W201"))
+        else:
+            newline_list = Linter.split_nl(final_token.text)
             num_nl = len(newline_list)
             if num_nl > 1:
                 for i in range(num_nl - 1):
-                    output.append((line_num - num_nl + 1 + i, "W300"))
+                    output.append((final_token.line + i + 1, "W300"))
         output.sort()
         return output
 
