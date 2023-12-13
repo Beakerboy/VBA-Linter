a4fff6c92b2eb3bb57003d0ce8bf403aeae4cf2d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 18:43:18 -0500

    Create e202.py

---
+++ src/vba_linter/rules/e202.py
@@ -0,0 +1,17 @@
+from antlr.vbaLexer import vbaLexer
+from vba_linter.rules.token_before_base import TokenBeforeBase
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='E202')
+
+
+class E202(TokenBeforeBase):
+    def __init__(self: T) -> None:
+        self._rule_name = "E202"
+        self._token_find = vbaLexer.NEWLINE
+        self._token_bad = vbaLexer.WS
+
+    def create_message(self: T, data: tuple) -> str:
+        output = TokenBeforeBase.create_message(self, data)
+        return output + "Unexpected whitespace bedore ')'"
