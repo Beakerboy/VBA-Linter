a0e62d039638bed8efa0bea38d1c6200064904f0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 15:02:39 -0500

    Create rule_stub.py

---
+++ tests/Unit/rule_stub.py
@@ -0,0 +1,21 @@
+from typing import TypeVar
+from vba_linter.rules.rule_base import Rulebase
+
+
+class RuleStub(RuleBase):
+
+    def __init__(self: T) -> None
+        self._output: list[tuple] = []
+        self._message = ''
+
+    def set_message(self: T, message: str) -> None:
+        self._message = message
+
+    def set_output(self:T, output: list) -> None:
+        self._output = output
+
+    def test(self: T, tokens: list) -> list
+        return self._output
+
+    def create_message(self: T, data: tuple) -> str:
+        return self._message
