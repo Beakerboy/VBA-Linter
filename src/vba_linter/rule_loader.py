d8c1523a55b5d380f19122fa2a6aaba8a8bea9d7
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 11:37:39 -0500

    Update rule_loader.py

--- src/vba_linter/rule_loader.py
+++ src/vba_linter/rule_loader.py
@@ -1,2 +1,19 @@
+from typing import TypeVar
+
+T = TypeVar('T', bound='RuleLoader')
+
+
 class RuleLoader:
-    pass
+    def __init__(self: T) -> None:
+        # scan specified rule directory
+        # scan ./rules
+        # merge list to allow users to override.
+        # create list of name to path
+        # load config file.
+        pass
+    
+    def test_all(self: T, tokens: list) -> list:
+        pass
+
+    def test_rule(self: T, tokens: list) -> list:
+        pass
