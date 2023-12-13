8f31535b14b043ac6cd87a37f3c52ed0d18fb371
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 18:40:14 -0500

    Create token_before_base.py

---
+++ src/vba_linter/rules/token_before_base.py
@@ -0,0 +1,19 @@
+from antlr.vbaLexer import vbaLexer
+from vba_linter.rules.rule_base import RuleBase
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='TokenBeforeBase')
+
+
+class TokenBeforeBase(RuleBase):
+
+    def test(self: T, tokens: list) -> list:
+        output: list[tuple] = []
+        prev_tok = None
+        for token in tokens:
+            if token.type == self._token_find:
+                if not (prev_tok is None) and prev_tok.type == self._token_bad:
+                    output.append((token.line, token.column, self._rule_name))
+            prev_tok = token
+        return output
