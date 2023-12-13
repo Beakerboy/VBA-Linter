3a99c2f5307dc12efd9f96d522e402c40b8a998f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 14:12:16 -0500

    Update test_w501.py

--- tests/Unit/test_w501.py
+++ tests/Unit/test_w501.py
@@ -1,13 +1,12 @@
-from antlr4 import InputStream
-from antlr.vbaLexer import vbaLexer
+from vba_linter.linter import Linter
 from vba_linter.rules.w501 import W501
 
 
 def test_line_length() -> None:
     code = ('Public Function Supercalifragilisticexpialidocious('
             'atrocious, precocious, indubitably)\r\nEnd Function\r\n')
-    input_stream = InputStream(code)
-    lexer = vbaLexer(input_stream)
+    linter = Linter()
+    lexer = linter.get_lexer(code)
     tokens = lexer.getAllTokens()
     rule = W501()
     assert rule.test(tokens) == [(1, 86, 'W501')]
