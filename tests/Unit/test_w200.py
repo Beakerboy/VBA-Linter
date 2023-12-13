6d90c46256efa8887b083bec8871612debe9f225
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 10:56:40 -0500

    Create test_w200.py

---
+++ tests/Unit/test_w200.py
@@ -0,0 +1,21 @@
+from antlr4 import InputStream
+from antlr.vbaLexer import vbaLexer
+from vba_linter.rules.w200 import W200
+
+
+eol_ws_data = [
+    (
+        'Public Function Foo(num) \r\nEnd Function\r\n',
+        [(1, "W200")]
+    ),
+]
+
+
+@pytest.mark.parametrize("code, expected", eol_ws_data)
+def test_eol_ws(code: str, expected: list) -> None:
+    input_stream = InputStream(code)
+    lexer = vbaLexer(input_stream)
+    tokens = lexer.getAllTokens()
+    rule = W200()
+    
+    assert rule.test(tokens) == expected
