82d5df1059a59facaaa30fad568c9f64c69a7064
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 10:31:27 -0500

    Update W200.py

--- src/vba_linter/rules/W200.py
+++ src/vba_linter/rules/W200.py
@@ -7,6 +7,16 @@ class W200(RuleBase):
     def __init__(self: T) -> None
         self.rule_name = "W200"
 
+    def test(self: T, tokens: list) -> list:
+        output = []
+        prev_tok = None
+        for token in tokens:
+            if token.type == vbaLexer.NEWLINE:
+                if not (prev_tok is None) and prev_tok.type == vbaLexer.WS:
+                    output.append((token.line, "W200"))
+            prev_tok = token
+        return output
+
     def create_message(self: T, data: tuple) -> str:
         output = RuleBase.create_message(self, data)
         return output + "Unexpected whitespace at the end of the line"
