965452e6175d66651a6c0d9a8f2ce70db544f4e6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 11:26:00 -0500

    Update rule_base.py

--- src/vba_linter/rules/rule_base.py
+++ src/vba_linter/rules/rule_base.py
@@ -1,3 +1,8 @@
+from typing import TypeVar
+
+T = TypeVar('T', bound='RuleBase')
+
+
 class RuleBase:
     def create_message(self: T, data: tuple) -> str:
         return ':' + str(data[0]) + ':' + str(data[1]) + ' '
