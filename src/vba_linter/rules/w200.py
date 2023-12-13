58c816f1c88c3b849ae2a78eadb53ec6141e69ce
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 18:40:53 -0500

    Update w200.py

--- src/vba_linter/rules/w200.py
+++ src/vba_linter/rules/w200.py
@@ -1,5 +1,5 @@
 from antlr.vbaLexer import vbaLexer
-from vba_linter.rules.rule_base import RuleBase
+from vba_linter.rules.token_before_base import TokwnBeforeBase
 from typing import TypeVar
 
 
@@ -11,16 +11,6 @@ class W200(RuleBase):
         self._rule_name = "W200"
         self._token_find = vbaLexer.NEWLINE
         self._token_bad = vbaLexer.WS
-
-    def test(self: T, tokens: list) -> list:
-        output: list[tuple] = []
-        prev_tok = None
-        for token in tokens:
-            if token.type == self._token_find:
-                if not (prev_tok is None) and prev_tok.type == self._token_bad:
-                    output.append((token.line, token.column, self._rule_name))
-            prev_tok = token
-        return output
 
     def create_message(self: T, data: tuple) -> str:
         output = RuleBase.create_message(self, data)
