4d5161adae8268cba827b9e497c814ff481d1603
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 14:48:08 -0500

    Update test_w500.py

--- tests/Unit/test_w500.py
+++ tests/Unit/test_w500.py
@@ -6,8 +6,8 @@ from vba_linter.rules.w500 import W500
 line_ending_data = [
     ('\r\n', []),
     ('\r\n\r\n', []),
-    ('\n\r\nFunction Foo()\r\n\r\nEnd Function\r\n', [(1, 1, "W500")]),
-    ('\r\n\nFunction Foo()\r\n\r\nEnd Function\r\n', [(2, 1, "W500")]),
+    ('\n\r\nFunction Foo()\r\n\r\nEnd Function\r\n', [(1, 0, "W500")]),
+    ('\r\n\nFunction Foo()\r\n\r\nEnd Function\r\n', [(2, 0, "W500")]),
     ('\r\n\r\nFoo\n', [(3, 3, "W500")]),
     (
         'Public Function Foo(num)\r\nEnd Function\n',
