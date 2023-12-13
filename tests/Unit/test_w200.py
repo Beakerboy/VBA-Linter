275a66d28b3ed9ed218e532e8413fe53204dd6dc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 11:41:57 -0500

    Update test_w200.py

--- tests/Unit/test_w200.py
+++ tests/Unit/test_w200.py
@@ -5,10 +5,18 @@ from vba_linter.rules.w200 import W200
 
 
 eol_ws_data = [
-    (
+    [
         'Public Function Foo(num) \r\nEnd Function\r\n',
         [(1, 25, "W200")]
-    ),
+    ],
+    [
+        'Public Function Foo(num)\r\n\r\nEnd Function \r\n',
+        [(3, 13, "W200")]
+    ],
+    [
+        'Public Function Foo(num)\r\n\nEnd Function \r\n',
+        [(3, 13, "W200")]
+    ],
 ]
 
 
