c09bbb76a7d208fbce8d6d270c6b1c9f81aefe5d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 15:20:31 -0500

    Update linter.py

--- src/vba_linter/linter.py
+++ src/vba_linter/linter.py
@@ -26,10 +26,7 @@ class Linter:
 
         # End of file checks
         final_token = tokens[-1]
-        if final_token is None or final_token.type != vbaLexer.NEWLINE:
-            line = 1 if final_token is None else final_token.line
-            output.append((line, "W201"))
-        else:
+        if not(final_token is None or final_token.type != vbaLexer.NEWLINE):
             newline_list = Linter.split_nl(final_token.text)
             num_nl = len(newline_list)
             if num_nl > 1:
