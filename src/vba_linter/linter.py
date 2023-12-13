3e5d5812e53c59e2c2bf67b32f1a393aae9afffc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 17:13:16 -0500

    Update linter.py

--- src/vba_linter/linter.py
+++ src/vba_linter/linter.py
@@ -27,23 +27,6 @@ class Linter:
         return output
 
     @classmethod
-    def split_nl(cls: Type[T], nl: str) -> list:
-        """
-        split a newline token into separate line end characters.
-        """
-        num = len(nl)
-        i = 0
-        result = []
-        while i < num:
-            if num >= 2 and nl[i:i+2] == '\r\n':
-                result.append('\r\n')
-                i += 2
-            else:
-                result.append(nl[i:i+1])
-                i += 1
-        return result
-
-    @classmethod
     def text_matches(cls: Type[T], pattern: str, name: str) -> bool:
         match = re.match(pattern, name)
         if match:
