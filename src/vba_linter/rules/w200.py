b9f95e2334ecc438adfbc4205c92ab433288699e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 18:36:21 -0500

    Update w200.py

--- src/vba_linter/rules/w200.py
+++ src/vba_linter/rules/w200.py
@@ -8,15 +8,17 @@ T = TypeVar('T', bound='W200')
 
 class W200(RuleBase):
     def __init__(self: T) -> None:
-        self.rule_name = "W200"
+        self._rule_name = "W200"
+        self._token_find = vbaLexer.NEWLINE
+        self._token_bad = vbaLexer.WS
 
     def test(self: T, tokens: list) -> list:
         output: list[tuple] = []
         prev_tok = None
         for token in tokens:
-            if token.type == vbaLexer.NEWLINE:
-                if not (prev_tok is None) and prev_tok.type == vbaLexer.WS:
-                    output.append((token.line, token.column, "W200"))
+            if token.type == self._token_find:
+                if not (prev_tok is None) and prev_tok.type == self._token_bad:
+                    output.append((token.line, token.column, self._rule_name))
             prev_tok = token
         return output
 
