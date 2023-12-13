3609d54ef5662ef86a08bf69e5ba52c594e727f6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 16:31:04 -0500

    Update rule_base.py

--- src/vba_linter/rules/rule_base.py
+++ src/vba_linter/rules/rule_base.py
@@ -9,3 +9,20 @@ class RuleBase:
 
     def create_message(self: T, data: tuple) -> str:
         return ':' + str(data[0]) + ':' + str(data[1]) + ' '
+
+    @classmethod
+    def split_nl(cls: Type[T], nl: str) -> list:
+        """
+        split a newline token into separate line end characters.
+        """
+        num = len(nl)
+        i = 0
+        result = []
+        while i < num:
+            if num >= 2 and nl[i:i+2] == '\r\n':
+                result.append('\r\n')
+                i += 2
+            else:
+                result.append(nl[i:i+1])
+                i += 1
+        return result
