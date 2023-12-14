7b68dcc2aa6bafb1e1e2a5f497db32232892b8c1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 09:00:33 -0500

    Update e203.py

--- src/vba_linter/rules/e203.py
+++ src/vba_linter/rules/e203.py
@@ -9,17 +9,7 @@ T = TypeVar('T', bound='E203')
 class E203(TokenBeforeBase):
     def __init__(self: T) -> None:
         self._rule_name = "E203"
-        self._token_find = vbaLexer.IDENTIFIER
+        self._token_find = vbaLexer.T__0
         self._token_bad = vbaLexer.WS
         self._message = "Whitespace before ','"
         self._find_value = ','
-
-    def test(self: T, tokens: list) -> list:
-        output: list[tuple] = []
-        for token in tokens:
-            line = token.line
-            column = token.column
-            name = token.type
-            text = token.text
-            output.append((line, column, name, text))
-        return output
