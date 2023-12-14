878c63da06aeb94ede1ad6ae21a56f4d3f047a79
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 06:15:52 -0500

    Create e201.py

---
+++ src/vba_linter/rules/e201.py
@@ -0,0 +1,14 @@
+from antlr.vbaLexer import vbaLexer
+from vba_linter.rules.token_before_base import TokenAfterBase
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='E201')
+
+
+class E201(TokenAfterBase):
+    def __init__(self: T) -> None:
+        self._rule_name = "E201"
+        self._token_find = vbaLexer.RPAREN
+        self._token_bad = vbaLexer.WS
+        self._message = "Unexpected whitespace after '('"
