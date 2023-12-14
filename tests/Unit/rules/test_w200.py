07098eddcb863f688d3f21f5a3971f8e6f1a7a43
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 19:23:37 -0500

    Update and rename tests/Unit/test_w200.py to tests/Unit/rules/test_w200.py

--- tests/Unit/rules/test_w200.py
+++ tests/Unit/rules/test_w200.py
@@ -3,7 +3,7 @@ from vba_linter.linter import Linter
 from vba_linter.rules.w200 import W200
 
 
-eol_ws_data = [
+test_data = [
     [
         'Public Function Foo(num) \r\nEnd Function\r\n',
         [(1, 25, "W200")]
@@ -19,11 +19,18 @@ eol_ws_data = [
 ]
 
 
-@pytest.mark.parametrize("code, expected", eol_ws_data)
-def test_eol_ws(code: str, expected: list) -> None:
+@pytest.mark.parametrize("code, expected", test_data)
+def test_test(code: str, expected: list) -> None:
     linter = Linter()
     lexer = linter.get_lexer(code)
     tokens = lexer.getAllTokens()
     rule = W200()
 
     assert rule.test(tokens) == expected
+
+
+def test_message() -> None:
+    rule = W200()
+    data = (3, 13, "W200")
+    expected = ":3:13 Unexpected whitespace at the end of the line"
+    assert rule.create_message(data) == expected
