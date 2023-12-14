d6b85d0c38a61efbd0f598f08d591da75b4a1ea1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 14:39:37 -0500

    Update and rename tests/Unit/test_w300.py to tests/Unit/rules/test_w391.py

---
+++ tests/Unit/rules/test_w391.py
@@ -0,0 +1,42 @@
+import pytest
+from typing import TypeVar
+from Unit.rules.rule_test_base import RuleTestBase
+from vba_linter.rules.w391 import W391
+
+
+T = TypeVar('T', bound='TestW391')
+
+anti_patterns = [
+    [
+        '''\
+Public Function Foo(num)
+End Function
+
+''',  # noqa
+        [(3, 0, "W300")]
+    ],
+    [
+        '''
+Public Function Foo(num)
+End Function
+
+
+''',  # noqa
+        [(3, 0, "W300"), (4, 0, "W300")]
+    ]
+]
+
+
+best_practice = [
+    ['''\
+Public Function Foo(num)
+End Function
+''',  # noqa
+     []
+]]
+
+
+@pytest.mark.parametrize("code, expected", anti_patterns + best_practice)
+class TestW291(RuleTestBase):
+    def __init__(self: T) -> None:
+        self.rule = W391()
