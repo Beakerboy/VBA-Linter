4b788e54e4183ee84ee11cb705d0f1a1a7f02cc4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 10:20:10 -0500

    Update e202.py

--- src/vba_linter/rules/e202.py
+++ src/vba_linter/rules/e202.py
@@ -9,9 +9,6 @@ T = TypeVar('T', bound='E202')
 class E202(TokenBeforeBase):
     def __init__(self: T) -> None:
         self._rule_name = "E202"
-        self._token_find = vbaLexer.RPAREN
-        self._token_bad = vbaLexer.WS
-
-    def create_message(self: T, data: tuple) -> str:
-        output = TokenBeforeBase.create_message(self, data)
-        return output + "Unexpected whitespace bedore ')'"
+        self._token_second = vbaLexer.RPAREN
+        self._token_first = vbaLexer.WS
+        self._message = "Whitespace before ')'"
