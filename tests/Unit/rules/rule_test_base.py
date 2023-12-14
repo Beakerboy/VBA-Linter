b6920e3390861cf25e5d4b34200352cff067ce5f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 13:04:18 -0500

    Update rule_test_base.py

--- tests/Unit/rules/rule_test_base.py
+++ tests/Unit/rules/rule_test_base.py
@@ -13,14 +13,14 @@ class RuleTestBase:
     
     @pytest.mark.parametrize("code, expected",
                              self.anti_patterns + self.best_practice)
-    def test_test(code: str, expected: list) -> None:
+    def test_test(self: T, code: str, expected: list) -> None:
         linter = Linter()
         lexer = linter.get_lexer(code)
         tokens = lexer.getAllTokens()
         assert self.rule.test(tokens) == expected
 
 
-    def test_message() -> None:
+    def test_message(self: T) -> None:
         data = (3, 13, "W291")
         expected = ":3:13: W291 trailing whitespace"
         assert self.rule.create_message(data) == expected
