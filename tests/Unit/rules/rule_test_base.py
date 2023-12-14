6ad74af814718af371887c7a78311f5a333e5c6a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 15:17:18 -0500

    Update rule_test_base.py

--- tests/Unit/rules/rule_test_base.py
+++ tests/Unit/rules/rule_test_base.py
@@ -15,16 +15,14 @@ End Function
          []]
     ]
 
-    def __init__(self: T) -> None:
-        self.rule: RuleBase
-
-    def test_test(self: T, code: str, expected: list) -> None:
+    def test_test(self: T, rule: RuleBase,
+                  code: str, expected: list) -> None:
         linter = Linter()
         lexer = linter.get_lexer(code)
         tokens = lexer.getAllTokens()
-        assert self.rule.test(tokens) == expected
+        assert rule.test(tokens) == expected
 
-    def test_message(self: T) -> None:
+    def test_message(self: T, rule: RuleBase) -> None:
         data = (3, 13, "W291")
         expected = ":3:13: W291 trailing whitespace"
-        assert self.rule.create_message(data) == expected
+        assert rule.create_message(data) == expected
