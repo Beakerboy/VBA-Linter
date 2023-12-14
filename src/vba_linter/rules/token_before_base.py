9bbc3964928c592bfb7567f371a1726181b1d6c9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 09:41:49 -0500

    Update token_before_base.py

--- src/vba_linter/rules/token_before_base.py
+++ src/vba_linter/rules/token_before_base.py
@@ -6,31 +6,21 @@ T = TypeVar('T', bound='TokenBeforeBase')
 
 
 class TokenBeforeBase(RuleBase):
-    """
-    Find a token of the given type, and the optional value.
-    If the precious token is of the given type, return the
-    line and column of the end of the previous token.
-    """
     def __init__(self: T, name: str,
-                 find: int, bad: int,
+                 second: int, first: int,
                  message: str) -> None:
         self._rule_name = name
-        self._token_find = find
-        self._token_bad = bad
+        self._token_second = second
+        self._token_first = first
         self._message = message
-        self._find_value = ""
-
-    def set_find_value(self: T, value: str) -> None:
-        self._find_value = value
-
+        
     def test(self: T, tokens: list) -> list:
         output: list[tuple] = []
         prev_tok = None
         for token in tokens:
-            value = token.text if self._find_value == '' else self._find_value
-            if token.type == self._token_find and token.text == value:
-                bad = self._token_bad
-                if not (prev_tok is None) and prev_tok.type == bad:
+            if token.type == self._token_second:
+                first = self._token_first
+                if not (prev_tok is None) and prev_tok.type == first:
                     line = token.line
                     column = token.column
                     name = self._rule_name
