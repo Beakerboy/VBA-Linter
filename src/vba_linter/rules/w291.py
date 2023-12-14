c5935b9efc02cd0f0e911a2a6d8fa57165b7d225
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 19:29:57 -0500

    Update and rename w200.py to w291.py

--- src/vba_linter/rules/w291.py
+++ src/vba_linter/rules/w291.py
@@ -3,11 +3,11 @@ from vba_linter.rules.token_before_base import TokenBeforeBase
 from typing import TypeVar
 
 
-T = TypeVar('T', bound='W200')
+T = TypeVar('T', bound='W291')
 
 
-class W200(TokenBeforeBase):
+class W291(TokenBeforeBase):
     def __init__(self: T) -> None:
         super().__init__(vbaLexer.NEWLINE, vbaLexer.WS)
         self._rule_name = "W200"
-        self._message = "Unexpected whitespace at the end of the line"
+        self._message = "trailing whitespace"
