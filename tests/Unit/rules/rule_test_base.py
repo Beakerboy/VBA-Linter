63f2514b4683cee253bbe523005aa96a6e7cb2df
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 13:15:46 -0500

    Update rule_test_base.py

--- tests/Unit/rules/rule_test_base.py
+++ tests/Unit/rules/rule_test_base.py
@@ -1,4 +1,3 @@
-import pytest
 from typing import TypeVar
 from vba_linter.linter import Linter
 from vba_linter.rules.rule_base import RuleBase
@@ -13,8 +12,6 @@ class RuleTestBase:
         self.best_practice = []
         self.rule: RuleBase
 
-    @pytest.mark.parametrize("code, expected",
-                             self.anti_patterns + self.best_practice)
     def test_test(self: T, code: str, expected: list) -> None:
         linter = Linter()
         lexer = linter.get_lexer(code)
