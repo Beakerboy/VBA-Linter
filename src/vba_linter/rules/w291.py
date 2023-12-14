24f4e6fcc9214a428c850ce58de6ea9480dc4e10
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 07:25:52 -0500

    Update w291.py

--- src/vba_linter/rules/w291.py
+++ src/vba_linter/rules/w291.py
@@ -8,6 +8,5 @@ T = TypeVar('T', bound='W291')
 
 class W291(TokenBeforeBase):
     def __init__(self: T) -> None:
-        super().__init__(vbaLexer.NEWLINE, vbaLexer.WS)
-        self._rule_name = "W291"
-        self._message = "trailing whitespace"
+        message = "trailing whitespace"
+        super().__init__("W291", vbaLexer.NEWLINE, vbaLexer.WS, message)
