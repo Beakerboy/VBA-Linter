f1ffb492f31095665eca66e20f2fd6321d2d81e4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 06:47:02 -0500

    Update token_after_base.py

--- src/vba_linter/rules/token_after_base.py
+++ src/vba_linter/rules/token_after_base.py
@@ -18,8 +18,8 @@ class TokenAfterBase(RuleBase):
         prev_tok = None
         for token in tokens:
             if token.type == self._token_bad:
-                prev_none = prev_tok is None
-                if not prev_none and prev_tok.type == self._token_find:
+                find = self._token_find
+                if not (prev_tok is None) and prev_tok.type == find:
                     name = self._rule_name
                     column = token.column + 1
                     output.append((token.line, column, name))
