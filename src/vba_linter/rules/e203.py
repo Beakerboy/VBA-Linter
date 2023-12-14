5545da5bfdbc1381972cbbfc351347c648559214
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 09:17:05 -0500

    Update e203.py

--- src/vba_linter/rules/e203.py
+++ src/vba_linter/rules/e203.py
@@ -12,4 +12,4 @@ class E203(TokenBeforeBase):
         self._token_find = vbaLexer.T__0
         self._token_bad = vbaLexer.WS
         self._message = "Whitespace before ','"
-        self._find_value = ','
+        # self._find_value = ','
