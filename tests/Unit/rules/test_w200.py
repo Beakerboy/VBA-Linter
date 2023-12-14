37bec00bd93fe155a48482c5ccca7529e0a6a25b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 20:43:09 -0500

    Update test_w200.py

--- tests/Unit/rules/test_w200.py
+++ tests/Unit/rules/test_w200.py
@@ -1,20 +1,20 @@
 import pytest
 from vba_linter.linter import Linter
-from vba_linter.rules.w200 import W200
+from vba_linter.rules.w291 import W291
 
 
 test_data = [
     [
         'Public Function Foo(num) \r\nEnd Function\r\n',
-        [(1, 25, "W200")]
+        [(1, 25, "W291")]
     ],
     [
         'Public Function Foo(num)\r\n\r\nEnd Function \r\n',
-        [(3, 13, "W200")]
+        [(3, 13, "W291")]
     ],
     [
         'Public Function Foo(num)\r\n\nEnd Function \r\n',
-        [(3, 13, "W200")]
+        [(3, 13, "W291")]
     ],
 ]
 
@@ -24,13 +24,13 @@ def test_test(code: str, expected: list) -> None:
     linter = Linter()
     lexer = linter.get_lexer(code)
     tokens = lexer.getAllTokens()
-    rule = W200()
+    rule = W291()
 
     assert rule.test(tokens) == expected
 
 
 def test_message() -> None:
     rule = W200()
-    data = (3, 13, "W200")
+    data = (3, 13, "W291")
     expected = ":3:13: W291 trailing whitespace"
     assert rule.create_message(data) == expected
