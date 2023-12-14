9c3794605c14d5b5446e4830acc02d358ac65128
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 18:58:39 -0500

    Update test_w291.py

--- tests/Unit/rules/test_w291.py
+++ tests/Unit/rules/test_w291.py
@@ -1,5 +1,4 @@
 import pytest
-from typing import TypeVar
 from Unit.rules.rule_test_base import RuleTestBase
 from vba_linter.rules.w291 import W291
 
@@ -31,17 +30,19 @@ End Function
 ]
 
 
-T = TypeVar('T', bound='TestW291')
+RuleTestBase.rule = W291()
 
 
-@pytest.mark.parametrize('rule', W291())
 @pytest.mark.parametrize(
     "code, expected",
     anti_patterns + RuleTestBase.best_practice
 )
-class TestW291(RuleTestBase):
-    def test_message(self: T) -> None:
-        data = (3, 13, "W291")
-        rule = W291()
-        expected = ":3:13: W291 trailing whitespace"
-        assert rule.create_message(data) == expected
+def test_test(code: str, expected: tuple) -> None:
+    RuleTestBase(code, expected)
+    
+
+def test_message() -> None:
+    data = (3, 13, "W291")
+    rule = W291()
+    expected = ":3:13: W291 trailing whitespace"
+    assert rule.create_message(data) == expected
