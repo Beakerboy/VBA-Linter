60ab536b8701802ad4d01898450968ddf34a4df6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 09:54:06 -0500

    Update token_after_base.py

--- src/vba_linter/rules/token_after_base.py
+++ src/vba_linter/rules/token_after_base.py
@@ -1,31 +1,21 @@
-from vba_linter.rules.rule_base import RuleBase
+from vba_linter.rules.token_before_base import TokenBeforeBase
 from typing import TypeVar
 
 
 T = TypeVar('T', bound='TokenAfterBase')
 
 
-class TokenAfterBase(RuleBase):
+class TokenAfterBase(TokenBeforeBase):
 
-    def __init__(self: T, find: int, bad: int) -> None:
+    def __init__(self: T, first: int, second: int) -> None:
         self._rule_name = ""
-        self._token_find = find
-        self._token_bad = bad
+        self._token_first = first
+        self._token_second = second
         self._message = ""
 
     def test(self: T, tokens: list) -> list:
+        temp = super().test(data)
         output: list[tuple] = []
-        prev_tok = None
-        for token in tokens:
-            if token.type == self._token_bad:
-                find = self._token_find
-                if not (prev_tok is None) and prev_tok.type == find:
-                    name = self._rule_name
-                    column = token.column + 1
-                    output.append((token.line, column, name))
-            prev_tok = token
+        for err in temp:
+            output.append((err[0], err[1] + 1, err[2]))
         return output
-
-    def create_message(self: T, data: tuple) -> str:
-        output = RuleBase.create_message(self, data)
-        return output + self._message
