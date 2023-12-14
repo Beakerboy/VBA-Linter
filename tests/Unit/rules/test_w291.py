1700e900ba4ee8cb6c3b4c0ca7b64625ad6efe3b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 13:15:12 -0500

    Update test_w291.py

--- tests/Unit/rules/test_w291.py
+++ tests/Unit/rules/test_w291.py
@@ -6,40 +6,44 @@ from vba_linter.rules.w291 import W291
 T = TypeVar('T', bound='TestW291')
 
 
-class TestW291(RuleTestBase):
-    def __init__(self: T) -> None:
-        self.rule = W291()
-        self.anti_patterns = [
-            [
-                '''\
+anti_patterns = [
+    [
+        '''\
 Public Function Foo(num) 
 End Function
 ''',  # noqa
-                [(1, 25, "W291")]
-            ],
-            [
-                '''\
+        [(1, 25, "W291")]
+    ],
+    [
+        '''\
 Public Function Foo(num)
 
 End Function 
 '''),  # noqa
-                [(3, 13, "W291")]
-            ],
-            [
-                '''\
+        [(3, 13, "W291")]
+    ],
+    [
+         '''\
 Public Function Foo(num)
  
 End Function
 ''',  # noqa
-                [(2, 1, "W291")]
-            ],
-        ]
+        [(2, 1, "W291")]
+    ],
+]
 
-        self.best_practice = [
-            ['''\
+
+best_practice = [
+    ['''\
 Public Function Foo(num)
 
 End Function
 ''',  # noqa
-             []]
-        ]
+       []]
+]
+
+
+@pytest.mark.parametrize("code, expected", anti_patterns + best_practice)
+class TestW291(RuleTestBase):
+    def __init__(self: T) -> None:
+        self.rule = W291()
