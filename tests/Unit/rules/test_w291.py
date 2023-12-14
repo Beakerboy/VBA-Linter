aefa4bb9051f402f4459dccdf05b53da4f98ad27
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 12:48:17 -0500

    Update test_w291.py

--- tests/Unit/rules/test_w291.py
+++ tests/Unit/rules/test_w291.py
@@ -3,55 +3,39 @@ from vba_linter.linter import Linter
 from vba_linter.rules.w291 import W291
 
 
-anti_patterns = [
-    [
-        '''\
+class TestW291(RuleTestBase):
+
+    anti_patterns = [
+        [
+            '''\
 Public Function Foo(num) 
 End Function
 ''',  # noqa
-        [(1, 25, "W291")]
-    ],
-    [
-        '''\
+            [(1, 25, "W291")]
+        ],
+        [
+            '''\
 Public Function Foo(num)
 
 End Function 
 '''),  # noqa
-        [(3, 13, "W291")]
-    ],
-    [
-        '''\
+            [(3, 13, "W291")]
+        ],
+        [
+            '''\
 Public Function Foo(num)
  
 End Function
 ''',  # noqa
-        [(2, 1, "W291")]
-    ],
-]
+            [(2, 1, "W291")]
+        ],
+    ]
 
-
-best_practice = [
-    ['''\
+    best_practice = [
+        ['''\
 Public Function Foo(num)
 
 End Function
 ''',  # noqa
-     []]
-]
-
-
-@pytest.mark.parametrize("code, expected", anti_patterns + best_practice)
-def test_test(code: str, expected: list) -> None:
-    linter = Linter()
-    lexer = linter.get_lexer(code)
-    tokens = lexer.getAllTokens()
-    rule = W291()
-
-    assert rule.test(tokens) == expected
-
-
-def test_message() -> None:
-    rule = W291()
-    data = (3, 13, "W291")
-    expected = ":3:13: W291 trailing whitespace"
-    assert rule.create_message(data) == expected
+         []]
+    ]
