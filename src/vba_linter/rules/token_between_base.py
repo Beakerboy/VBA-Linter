a869441fa846561c740ca796f6c273b41785bd14
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-15 15:31:24 -0500

    Update token_between_base.py

--- src/vba_linter/rules/token_between_base.py
+++ src/vba_linter/rules/token_between_base.py
@@ -20,11 +20,12 @@ class TokenBetweenBase(RuleBase):
         tok1 = tokens[0]
         tok2 = tokens[1]
         for token in tokens[2:]:
-            if (token.type == self._token_third and
-                    tok1 == self._token_first and
-                    tok2 == self._token_second):
-                line = token.line
-                column = token.column
+            if True:
+            # if (token.type == self._token_third and
+            #       tok1 == self._token_first and
+            #       tok2 == self._token_second):
+                line = token.text
+                column = token.type
                 name = self._rule_name
                 output.append((line, column, name))
             tok1 = tok2
