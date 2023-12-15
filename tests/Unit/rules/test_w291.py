16d00bf86ecf55468fbc2ad2446d13a8762ef27d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-15 10:58:40 -0500

    Update test_w291.py

--- tests/Unit/rules/test_w291.py
+++ tests/Unit/rules/test_w291.py
@@ -46,7 +46,7 @@ rule = W291()
     anti_patterns + RuleTestBase.best_practice
 )
 def test_test(rule: RuleBase, code: str, expected: tuple) -> None:
-    RuleTestBase.test_test(rule, code, expected)
+    assert RuleTestBase.tokenize(rule, code) == expected
 
 
 @pytest.mark.parametrize('rule', [rule])
