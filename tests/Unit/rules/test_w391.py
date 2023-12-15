bf73b419bd22d33ef90deb322d6abfe2740c88c0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-15 07:44:34 -0500

    Update test_w391.py

--- tests/Unit/rules/test_w391.py
+++ tests/Unit/rules/test_w391.py
@@ -1,5 +1,4 @@
 import pytest
-from vba_linter.linter import Linter
 from vba_linter.rules.rule_base import RuleBase
 from Unit.rules.rule_test_base import RuleTestBase
 from vba_linter.rules.w391 import W391
@@ -44,14 +43,9 @@ rule = W391()
     anti_patterns + RuleTestBase.best_practice
 )
 def test_test(rule: RuleBase, code: str, expected: tuple) -> None:
-    # RuleTestBase.test_test(rule, code, expected)
-    linter = Linter()
-    lexer = linter.get_lexer(code)
-    tokens = lexer.getAllTokens()
-    assert rule.test(tokens) == expected
+    RuleTestBase.test_test(rule, code, expected)
 
 
-@pytest.mark.parametrize('rule', [rule])
 def test_message(rule: RuleBase) -> None:
     data = (3, 13, "W391")
     expected = ":3:13: W391 blank line at end of file"
