760861821088eed2a90512a21b2d6ba8caf3406f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-14 22:07:49 -0500

    Update test_w291.py

--- tests/Unit/rules/test_w291.py
+++ tests/Unit/rules/test_w291.py
@@ -1,5 +1,6 @@
 import pytest
 from Unit.rules.rule_test_base import RuleTestBase
+from vba_linter.rules.RuleBase import RuleBase
 from vba_linter.rules.w291 import W291
 
 
@@ -30,15 +31,15 @@ End Function
 ]
 
 
-RuleTestBase.rule = W291()
+rule = W291()
 
-
+@pytest.mark.parametrize('rule', rule)
 @pytest.mark.parametrize(
     "code, expected",
     anti_patterns + RuleTestBase.best_practice
 )
-def test_test(code: str, expected: tuple) -> None:
-    RuleTestBase.test_test(code, expected)
+def test_test(rule: RuleBase, code: str, expected: tuple) -> None:
+    RuleTestBase.test_test(rule, code, expected)
 
 
 def test_message() -> None:
