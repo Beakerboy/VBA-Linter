a46fd19dcf4e938e30e155f882b1352e4ddb3d93
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-15 15:23:24 -0500

    Update and rename test_e211.pyt to test_e211.py

---
+++ tests/Unit/rules/test_e211.py
@@ -0,0 +1,48 @@
+import pytest
+from Unit.rules.rule_test_base import RuleTestBase
+from vba_linter.rules.rule_base import RuleBase
+from vba_linter.rules.e211 import E211
+
+
+anti_patterns = [
+    [
+        '''\
+Public Function Foo (num)
+End Function
+''',  # noqa
+        [(1, 20, "E211")]
+    ],
+    [
+        '''\
+Public Function Foo(data)
+    bar = data (1)
+End Function
+''',  # noqa
+        [(2, 15, "E211")]
+    ],
+]
+
+
+message_data = [
+    [(1, 20, "E211"), ":1:20: E211 Whitespace before '('"],
+ ]
+
+
+rule = E211()
+
+
+@pytest.mark.parametrize('rule', [rule])
+@pytest.mark.parametrize(
+    "code, expected",
+    anti_patterns + RuleTestBase.best_practice
+)
+def test_test(rule: RuleBase, code: str, expected: tuple) -> None:
+    assert RuleTestBase.tokenize(rule, code) == expected
+
+
+@pytest.mark.parametrize('rule', [rule])
+@pytest.mark.parametrize(
+    "data, expected", message_data
+)
+def test_message(rule: RuleBase, data: tuple, expected: str) -> None:
+    assert rule.create_message(data) == expected
