7813e1432b783de0488d36d4a7837995bf0a3e97
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-15 09:10:14 -0500

    Create test_rule_base.py (#29)

---
+++ tests/Unit/rules/test_rule_base.py
@@ -0,0 +1,11 @@
+from vba_linter.rules.rule_base import RuleBase
+
+
+def test_constructor() -> None:
+    base = RuleBase()
+    assert isinstance(base, RuleBase)
+
+
+def test_test() -> None:
+    base = RuleBase()
+    assert base.test([]) == []
