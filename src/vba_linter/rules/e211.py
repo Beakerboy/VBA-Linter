75e4460492c8264881b357e8b55aa1e9e3286e1a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-15 15:24:36 -0500

    Update e211.py

--- src/vba_linter/rules/e211.py
+++ src/vba_linter/rules/e211.py
@@ -9,7 +9,7 @@ T = TypeVar('T', bound='E211')
 class E211(TokenBeforeBase):
     def __init__(self: T) -> None:
         self._rule_name = "E203"
-        self._token_first = vbaLexer.FUNCTION
+        self._token_first = vbaLexer.IDENTIFIER
         self._token_second = vbaLexer.WS
         self._token_third = vbaLexer.LPAREN
         self._message = "whitespace before '('"
