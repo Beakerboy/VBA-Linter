8130e4db19054596b6bd0f3915b9ceb4f633d293
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-15 11:51:59 -0500

    Create test_w191.py

---
+++ tests/Unit/rules/test_w191.py
@@ -0,0 +1,41 @@
+import pytest
+from Unit.rules.rule_test_base import RuleTestBase
+from vba_linter.rules.rule_base import RuleBase
+from vba_linter.rules.w191 import W191
+
+
+anti_patterns = [
+    [
+        '''\
+Public Function Foo(num)
+\tBar = 2
+End Function
+''',  # noqa
+        [(2, 1, "W191")]
+    ],
+]
+
+
+message_data = [
+    [(2, 1, "W191"), ":2:1: W191 indentation contains tabs"],
+ ]
+
+
+rule = W191()
+
+
+@pytest.mark.parametrize('rule', [rule])
+@pytest.mark.parametrize(
+    "code, expected",
+    anti_patterns + RuleTestBase.best_practice
+)
+def test_test(rule: RuleBase, code: str, expected: tuple) -> None:
+    assert RuleTestBase.tokenize(rule, code) == expected
+
+
+@pytest.mark.parametrize('rule', [rule])
+@pytest.mark.parametrize(
+    "data, expected", message_data
+)
+def test_message(rule: RuleBase, data: tuple, expected: str) -> None:
+    assert rule.create_message(data) == expected
