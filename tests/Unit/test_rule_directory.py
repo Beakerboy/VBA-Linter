d6fff76e394909488592765060cd2619b5e3e60b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-16 07:20:20 -0500

    Update test_rule_directory.py

--- tests/Unit/test_rule_directory.py
+++ tests/Unit/test_rule_directory.py
@@ -1,5 +1,6 @@
 from vba_linter.rule_directory import RuleDirectory
 from Unit.rule_stub import RuleStub
+from vba_linter.linter import Linter
 
 
 def test_constructor() -> None:
@@ -15,6 +16,8 @@ def test_add() -> None:
     assert rule1.test_count == 1
     rule2 = RuleStub()
     obj.add_rule(rule2)
-    obj.test_all()
+
+    linter = Linter()
+    obj.test_all(linter.get_lexer(code).getAllTokens())
     assert rule1.test_count == 2
     assert rule2.test_count == 1
