d63d94ce31879a4059296f2824a8e2de2e4831c3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-15 23:20:58 -0500

    Update test_rule_directory.py

--- tests/Unit/test_rule_directory.py
+++ tests/Unit/test_rule_directory.py
@@ -9,7 +9,13 @@ def test_constructor() -> None:
 
 def test_add() -> None:
     obj = RuleDirectory()
-    rule = RuleStub()
+    rule1 = RuleStub()
     a = len(obj.get_rules())
-    obj.add_rule(rule)
-    assert len(obj.get_rules()) == a + 1
+    obj.add_rule(rule1)
+    obj.test_all()
+    assert rule1.test_count == 1
+    rule2 = RuleStub()
+    obj.add_rule(rule2)
+    obj.test_all()
+    assert rule1.test_count == 2
+    assert rule2.test_count == 1
