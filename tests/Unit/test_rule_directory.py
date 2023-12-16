bf2de627b35e1400b2aaf456508f0e6ccb2c0608
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-15 22:05:46 -0500

    Update test_rule_directory.py

--- tests/Unit/test_rule_directory.py
+++ tests/Unit/test_rule_directory.py
@@ -1,6 +1,13 @@
 from vba_linter.rule_directory import RuleDirectory
-
+from Unit.rule_stub import RuleStub
 
 def test_constructor() -> None:
     obj = RuleDirectory()
     assert isinstance(obj, RuleDirectory)
+
+def test_add() -> None:
+    obj = RuleDirectory()
+    rule = RuleStub()
+    a = len(obj.get_rules())
+    obj.add_rule(rule)
+    assert len(obj.get_rules()) == a + 1
