722df99fad4a5c778ec3fbce41e22328499d592e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-16 07:27:21 -0500

    Update test_rule_directory.py

--- tests/Unit/test_rule_directory.py
+++ tests/Unit/test_rule_directory.py
@@ -11,15 +11,15 @@ def test_constructor() -> None:
 def test_add() -> None:
     obj = RuleDirectory()
     rule1 = RuleStub()
+    linter = Linter()
     obj.add_rule(rule1)
     assert rule1.test_count == 0
-    obj.test_all()
+    obj.test_all(linter.get_lexer(code).getAllTokens())
     assert rule1.test_count == 1
     rule2 = RuleStub()
     obj.add_rule(rule2)
 
     code = ""
-    linter = Linter()
     obj.test_all(linter.get_lexer(code).getAllTokens())
     assert rule1.test_count == 2
     assert rule2.test_count == 1
