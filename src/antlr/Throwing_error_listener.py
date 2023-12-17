4e2d7ffe4f0da2b6ce692771ccb17ad044032467
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-17 17:56:43 -0500

    Create Throwing_error_listener.py

---
+++ src/antlr/Throwing_error_listener.py
@@ -0,0 +1,9 @@
+from antlr4.error.ErrorListener import ErrorListener
+from antlr4.error.Errors import ParseCancellationException
+
+class ThrowingErrorListener(ErrorListener):
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        ex = ParseCancellationException(f'line {line}: {column} {msg}')
+        ex.line = line
+        ex.column = column
+        raise ex
