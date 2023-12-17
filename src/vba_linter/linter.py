e20bcdeb1c293d525b0e5334540419970e38e863
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-17 15:14:29 -0500

    Update linter.py

--- src/vba_linter/linter.py
+++ src/vba_linter/linter.py
@@ -24,7 +24,9 @@ class Linter:
         lexer = self.get_lexer(code)
         stream = CommonTokenStream(lexer)
         parser = vbaParser(stream)
-        tree = parser.startRule()
+        if parser.getNumberOfSyntaxErrors() > 0:
+            return [(x, x, "E999")]
+        # tree = parser.startRule()
         # if check lost option is set
         # check for lint errors
         lexer = self.get_lexer(code)
