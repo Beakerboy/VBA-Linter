367a088f3eb59417a1e634e5dfc7ffea594d6fb5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-18 11:01:19 -0500

    Update test_e201.py

--- tests/Unit/rules/test_e201.py
+++ tests/Unit/rules/test_e201.py
@@ -4,7 +4,6 @@ from Unit.rules.rule_test_base import RuleTestBase
 from vba_linter.linter import Linter
 from vba_linter.rule_directory import RuleDirectory
 from vba_linter.rules.rule_base import RuleBase
-from vba_linter.rules.token_after_base import TokenAfterBase
 
 
 anti_patterns = [
