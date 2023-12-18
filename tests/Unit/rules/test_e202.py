b6d2cc7c00f65b5f4fb383d2ad7c0b8a030cff6d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-18 10:20:43 -0500

    Update test_e202.py

--- tests/Unit/rules/test_e202.py
+++ tests/Unit/rules/test_e202.py
@@ -1,6 +1,8 @@
 import pytest
 from vba_linter.linter import Linter
 from vba_linter.rule_directory import RuleDirectory
+from Unit.rules.rule_test_base import RuleTestBase
+from vba_linter.rules.rule_base import RuleBase
 
 
 anti_patterns = [
