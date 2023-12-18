68a087e14bcfae5ea749342a25a14859473ad042
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-18 15:52:34 -0500

    Update and rename tests/Unit/test_w201.py to tests/Unit/rules/test_w201.py

---
+++ tests/Unit/rules/test_w201.py
@@ -0,0 +1,24 @@
+import pytest
+from vba_linter.rules.rule_base import RuleBase
+from vba_linter.rules.w201 import W201
+from Unit.rules.rule_test_base import RuleTestBase
+
+
+anti_patterns = [
+    ('''\
+Public Function Foo(num)
+nEnd Function''',  # noqa
+    [(2, 12, 'W201')]
+]
+
+
+rule = W201()
+
+
+pytest.mark.parametrize('rule', [rule])
+@pytest.mark.parametrize(
+    "code, expected",
+    anti_patterns + RuleTestBase.best_practice
+)
+def test_test(rule: RuleBase, code: str, expected: tuple) -> None:
+    assert RuleTestBase.tokenize(rule, code) == expected
