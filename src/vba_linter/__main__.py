14951f9f3d947fd62b3bffde9be45216478cacbe
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-18 17:39:05 -0500

    Update __main__.py

--- src/vba_linter/__main__.py
+++ src/vba_linter/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 from vba_linter.linter import Linter
 from vba_linter.rule_directory import RuleDirectory
 
@@ -14,13 +15,17 @@ def main() -> None:
     linter = Linter()
     file_list = find_files(args.directory)
     full_results: dict[str, list] = {}
-    for file in file_list:
+    for file_name in file_list:
         code = open(file, 'r').read()
         dir = RuleDirectory()
         dir.load_all_rules()
-        results = linter.lint(dir, code)
+        results = linter.lint(dir, file_name)
         if len(results) > 0:
-            full_results[file] = results
+            full_results[file_name] = results
+    for file_name, file_results in full_results.items():
+        for error in file_results:
+            msg = dir.get_rule(error[2]).create_message(error)
+            print(file_name + msg, file=sys.stderr)
 
 
 def find_files(path: str) -> list:
