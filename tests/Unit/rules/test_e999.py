5b27c5077509163137592d3a77e32a13402f50a3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-17 21:41:47 -0500

    Update test_e999.py

--- tests/Unit/rules/test_e999.py
+++ tests/Unit/rules/test_e999.py
@@ -9,11 +9,41 @@ anti_patterns = [
 Public Function Foo(num)
 End Sub
 ''',  # noqa
-        [('x', 'x', "E999")]
+        [('x', 'x', "E999",
+          "mismatched input 'End Sub' expecting {'.', '!', 'ACCESS', " +
+          "'ADDRESSOF', 'ALIAS', 'AND', 'ATTRIBUTE', 'APPACTIVATE', " +
+          "'APPEND', 'AS', 'BEGIN', 'BEEP', 'BINARY', 'BOOLEAN', " +
+          "'BYVAL', 'BYREF', 'BYTE', 'CALL', 'CASE', 'CHDIR', " +
+          "'CHDRIVE', 'CLASS', 'CLOSE', 'COLLECTION', 'CONST', " +
+          "'DATABASE', 'DATE', 'DECLARE', 'DEFBOOL', 'DEFBYTE', " +
+          "'DEFDATE', 'DEFDBL', 'DEFDEC', 'DEFCUR', 'DEFINT', " +
+          "'DEFLNG', 'DEFOBJ', 'DEFSNG', 'DEFSTR', 'DEFVAR', " +
+          "'DELETESETTING', 'DIM', 'DO', 'DOUBLE', 'EACH', 'ELSE', " +
+          "'ELSEIF', END_FUNCTION, 'END', 'ENUM', 'EQV', 'ERASE', " +
+          "'ERROR', 'EVENT', EXIT_DO, EXIT_FOR, EXIT_FUNCTION, " +
+          "EXIT_PROPERTY, EXIT_SUB, 'FALSE', 'FILECOPY', 'FRIEND', " +
+          "'FOR', 'FUNCTION', 'GET', 'GLOBAL', 'GOSUB', 'GOTO', 'IF', " +
+          "'IMP', 'IMPLEMENTS', 'IN', 'INPUT', 'IS', 'INTEGER', " +
+          "'KILL', 'LOAD', 'LOCK', 'LONG', 'LOOP', 'LEN', 'LET', " +
+          "'LIB', 'LIKE', LINE_INPUT, 'LSET', '#CONST', '#IF', 'ME', "
+          "'MID', 'MKDIR', 'MOD', 'NAME', 'NEXT', 'NEW', 'NOT', " +
+          "'NOTHING', 'NULL', 'ON', ON_ERROR, ON_LOCAL_ERROR, 'OPEN', " +
+          "'OPTIONAL', 'OR', 'OUTPUT', 'PARAMARRAY', 'PRESERVE', " +
+          "'PRINT', 'PRIVATE', 'PUBLIC', 'PUT', 'RANDOM', " +
+          "'RANDOMIZE', 'RAISEEVENT', 'READ', 'REDIM', 'REM', " +
+          "'RESET', 'RESUME', 'RETURN', 'RMDIR', 'RSET', " +
+          "'SAVEPICTURE', 'SAVESETTING', 'SEEK', 'SELECT', " +
+          "'SENDKEYS', 'SET', 'SETATTR', 'SHARED', 'SINGLE', 'SPC', " +
+          "'STATIC', 'STEP', 'STOP', 'STRING', 'SUB', 'TAB', 'TEXT', " +
+          "'THEN', 'TIME', 'TO', 'TRUE', 'TYPE', 'TYPEOF', 'UNLOAD', " +
+          "'UNLOCK', 'UNTIL', 'VARIANT', 'VERSION', 'WEND', 'WHILE', " +
+          "'WIDTH', 'WITH', 'WITHEVENTS', 'WRITE', 'XOR', " +
+          "SHORTLITERAL, INTEGERLITERAL, LINE_CONTINUATION, IDENTIFIER}"
+         )]
     ],
     [
         '<?php phpinfo() ?>',
-        [('x', 'x', "E999")]
+        [('x', 'x', "E999", "mismatched input '<?php' expecting <EOF>")]
     ],
 ]
 
