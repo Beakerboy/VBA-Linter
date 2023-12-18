9ab1efddd59c645c3ba2046843ed804a0fb0c685
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-18 10:23:20 -0500

    Update rule_directory.py

--- src/vba_linter/rule_directory.py
+++ src/vba_linter/rule_directory.py
@@ -4,6 +4,8 @@ from vba_linter.rules.rule_base import RuleBase
 from vba_linter.rules.w291 import W291
 from vba_linter.rules.w201 import W201
 from vba_linter.rules.token_after_base import TokenAfterBase
+from vba_linter.rules.token_before_base import TokenBeforeBase
+from vba_linter.rules.token_between_base import TokenBetweenBase
 from vba_linter.rules.w391 import W391
 from vba_linter.rules.w500 import W500
 from vba_linter.rules.w501 import W501
@@ -35,6 +37,9 @@ class RuleDirectory:
         e203 = TokenBeforeBase("E203",
                                vbaLexer.WS, vbaLexer.T__0
                                "Whitespace before ','")
+        e211 = TokenBetweenBase("E211", vbaLexer.IDENTIFIER, vbaLexer.WS,
+                                vbaLexer.LPAREN, "whitespace before '('")
+        )
         self._rules.update({"E201" : e201, "E202" : e202, "E203" : e203})
         self._rules.update({"W291" : W291(), "W201" : W201(),
                             "W391" : W391(), "W500" : W500(),
