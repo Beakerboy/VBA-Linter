cc0b82bb40e8d83c4ed7e450b47c602a8a5c03af
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-17 22:27:11 -0500

    Update thrown_exception.py

--- src/antlr/thrown_exception.py
+++ src/antlr/thrown_exception.py
@@ -3,7 +3,7 @@ from antlr4.error.Errors import ParseCancellationException
 class ThrownException(ParseCancellationException):
 
     def __init__(self, message):
-        super().__init(message)
+        super().__init__(message)
         self.msg = ''
         self.line = 0
         self.column = 0
