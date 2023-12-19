47502694eb8100db796266b347b6fd67fa4d7d72
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-18 19:22:50 -0500

    Update test_main.py

--- tests/Unit/test_main.py
+++ tests/Unit/test_main.py
@@ -3,7 +3,7 @@ from vba_linter.__main__ import main
 
 
 def test_bad_file(mocker: MockerFixture) -> None:
-    dir_path = "tests/Files"
+    dir_path = "tests/Files/project"
     mocker.patch(
         "sys.argv",
         [
