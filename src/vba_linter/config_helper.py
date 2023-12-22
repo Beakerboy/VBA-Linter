88b0268377890f5eefb855dfa3dac976abb2375a
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2023-12-22 00:43:15 -0500

    Add editor config

---
+++ src/vba_linter/config_helper.py
@@ -0,0 +1,11 @@
+from editorconfig import get_properties, EditorConfigError
+
+filename = "/linter.py"
+
+try:
+    options = get_properties(filename)
+except EditorConfigError:
+    print("Error occurred while getting EditorConfig properties")
+else:
+    for key, value in options.items():
+        print("%s=%s" % (key, value))
