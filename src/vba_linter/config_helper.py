5ce89b362e664a652c791da8926595ca6829bc16
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2023-12-22 01:29:02 -0500

    Add config helper

---
+++ src/vba_linter/config_helper.py
@@ -0,0 +1,15 @@
+from editorconfig import get_properties, EditorConfigError
+
+
+T = TypeVar('T', bound='ConfigHelper')
+
+class ConfigHelper:
+    def get_property(filename: str, property_name: str)
+        try:
+            options: dict = get_properties(filename)
+        except EditorConfigError:
+            print("Error occurred while getting EditorConfig properties")
+        else:
+            for key, value in options.items():
+                if (key == property_name):
+                    return value
