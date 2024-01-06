8b67bc4dd02fe84ecf1a05941512fc090e2cf22a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-06 06:32:31 -0500

    Update and rename w500.py to line_ending.py

--- src/vba_linter/rules/line_ending.py
+++ src/vba_linter/rules/line_ending.py
@@ -1,6 +1,6 @@
 from antlr4_vba.vbaLexer import vbaLexer
 from vba_linter.rules.rule_base import RuleBase
-from typing import TypeVar
+from typing import LTypeVar
 
 
 T = TypeVar('T', bound='W500')
