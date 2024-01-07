b88de6da0568ff7acba79cefd275eb3eb69a6f86
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-07 17:28:19 -0500

    Update blank_line_eof.py

--- src/vba_linter/rules/blank_line_eof.py
+++ src/vba_linter/rules/blank_line_eof.py
@@ -19,7 +19,7 @@ class BlankLineEof(RuleBase):
         output: List[tuple] = []
         if (ts.index > 0 and ts.LA(1) == vbaLexer.NEWLINE and
                 ts.LA(2) == Token.EOF):
-            final_token = ts.LT(-1)
+            final_token = ts.LT(1)
             newline_list = RuleBase.split_nl(final_token.text)
             num_nl = len(newline_list)
             if num_nl > 1:
