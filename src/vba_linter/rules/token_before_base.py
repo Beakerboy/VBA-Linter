6f1bdb917ccc1394994b39c0b5bac0b5b313dc42
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-07 12:15:06 -0500

    Update token_before_base.py

--- src/vba_linter/rules/token_before_base.py
+++ src/vba_linter/rules/token_before_base.py
@@ -1,3 +1,4 @@
+from antlr4 import Token
 from antlr4_vba.vbaLexer import vbaLexer
 from vba_linter.rules.rule_base import RuleBase
 from typing import List, TypeVar
@@ -7,6 +8,8 @@ T = TypeVar('T', bound='TokenBeforeBase')
 
 
 class TokenBeforeBase(RuleBase):
+    """
+    """
     def __init__(self: T, name: str,
                  first: int, second: int,
                  message: str) -> None:
@@ -15,18 +18,14 @@ class TokenBeforeBase(RuleBase):
         self._token_first = first
         self._message = message
 
-    def test(self: T, lexer: vbaLexer) -> list:
-        tokens = lexer.getAllTokens()
+    def test(self: T, ts: BufferedTokenStream) -> list:
         output: List[tuple] = []
-        if len(tokens) < 2:
+        token_stream.p < 2
             return output
-        prev_tok = tokens[0]
-        for token in tokens[1:]:
-            if (prev_tok.type == self._token_first and
-                    token.type == self._token_second):
-                line = token.line
-                column = token.column
-                name = self._rule_name
-                output.append((line, column, name))
-            prev_tok = token
+        if (ts.LT(1) == self._token_second and
+                ts.LT(-1) == self._token_first):
+            line = token.line
+            column = token.column
+            name = self._rule_name
+            output.append((line, column, name))
         return output
