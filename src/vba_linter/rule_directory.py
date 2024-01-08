83fbd58afb446d6a6337da9bab2956d34c34ddf5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-07 22:01:02 -0500

    Update rule_directory.py

--- src/vba_linter/rule_directory.py
+++ src/vba_linter/rule_directory.py
@@ -46,6 +46,7 @@ class RuleDirectory:
         self._rules.update({"W291": TrailingWhitespace(), "W201": NewlineEof(),
                             "W391": BlankLineEof(), "W500": LineEnding(),
                             "W501": LineTooLong(), "E101": MixedIndent()})
+
     def get_rule(self: T, rule_name: str) -> RuleBase:
         if rule_name == "E999":
             return ParsingError()
