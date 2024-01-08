9516470a590c2b8b377c3c725dc22d97d2cccb28
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-07 21:07:21 -0500

    Update line_ending.py

--- src/vba_linter/rules/line_ending.py
+++ src/vba_linter/rules/line_ending.py
@@ -1,3 +1,4 @@
+from antlr4 import CommonTokenStream
 from antlr4_vba.vbaLexer import vbaLexer
 from vba_linter.rules.rule_base import RuleBase
 from typing import List, TypeVar
@@ -12,15 +13,14 @@ class LineEnding(RuleBase):
         self._line_ending = '\r\n'
         self._message = 'incorrect line ending'
 
-    def test(self: T, lexer: vbaLexer) -> list:
-        tokens = lexer.getAllTokens()
+    def test(self: T, ts: CommonTokenStream) -> list:
         output: List[tuple] = []
-        for token in tokens:
-            if token.type == vbaLexer.NEWLINE:
-                newline_list = RuleBase.split_nl(token.text)
-                num_nl = len(newline_list)
-                for i in range(num_nl):
-                    if newline_list[i] != self._line_ending:
-                        column = token.column if i == 0 else 0
-                        output.append((token.line + i, column, "W500"))
+        token = ts.LT(1)
+        if token.type == vbaLexer.NEWLINE:
+            newline_list = RuleBase.split_nl(token.text)
+            num_nl = len(newline_list)
+            for i in range(num_nl):
+                if newline_list[i] != self._line_ending:
+                    column = token.column if i == 0 else 0
+                    output.append((token.line + i, column, "W500"))
         return output
