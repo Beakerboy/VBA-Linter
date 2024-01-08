f40502ecbd012b19f683354a8144468500f3f074
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-07 20:48:01 -0500

    Update mixed_indent.py

--- src/vba_linter/rules/mixed_indent.py
+++ src/vba_linter/rules/mixed_indent.py
@@ -1,3 +1,4 @@
+from antlr4 import CommonTokenStream
 from antlr4_vba.vbaLexer import vbaLexer
 from vba_linter.rules.rule_base import RuleBase
 from typing import List, TypeVar
@@ -15,24 +16,23 @@ class MixedIndent(RuleBase):
         self._rule_name = 'E101'
         self._message = "indentation contains mixed spaces and tabs"
 
-    def test(self: T, lexer: vbaLexer) -> list:
-        tokens = lexer.getAllTokens()
+    def test(self: T, ts: CommonTokenStream) -> list:
         output: List[tuple] = []
-        for token in tokens:
-            if token.type == vbaLexer.WS and token.column == 0:
-                # if next token exists and is not NEWLINE
-                # should the scope be checked to decide if this
-                # is unexpeced indentation?
-                has_tab = False
-                has_space = False
-                for char in token.text:
-                    if char == '\t':
-                        has_tab = True
-                    if char == ' ':
-                        has_space = True
-                    if has_tab and has_space:
-                        line = token.line
-                        rule = self._rule_name
-                        output.append((line, 1, rule))
-                        break
+        token = ts.LT(1)
+        if token.type == vbaLexer.WS and token.column == 0:
+            # if next token exists and is not NEWLINE
+            # should the scope be checked to decide if this
+            # is unexpeced indentation?
+            has_tab = False
+            has_space = False
+            for char in token.text:
+                if char == '\t':
+                    has_tab = True
+                if char == ' ':
+                    has_space = True
+                if has_tab and has_space:
+                    line = token.line
+                    rule = self._rule_name
+                    output = [(line, 1, rule)]
+                    break
         return output
