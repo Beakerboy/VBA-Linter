a4ced0ba8931ce4eb27a4ed9274f8e7379304314
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-08 10:36:29 -0500

    Update rule_test_base.py

--- tests/Unit/rules/rule_test_base.py
+++ tests/Unit/rules/rule_test_base.py
@@ -23,16 +23,11 @@ class RuleTestBase:
         ]
     ]
 
-    worst_practice = [
-        [
-            '''\
+    worst_practice = '''\
 Public Function Supercalifragilisticexpialidocious ( atrocious , precocious, indubitably ) 
 \tbar  =  (2+1)\r\nEnd Function
 ''',  # noqa
-            [
-            ]
-        ]
-    ]
+
 
     @classmethod
     def save_code(cls: Type[T], code: str) -> str:
