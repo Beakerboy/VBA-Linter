dad92595702e9e6cd764edf9443e7e525a39f8b2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-08 18:01:13 -0500

    Update ambiguous_identifier.py

--- src/vba_linter/rules/ambiguous_identifier.py
+++ src/vba_linter/rules/ambiguous_identifier.py
@@ -1,25 +1,19 @@
 from antlr4 import CommonTokenStream, Token
 from antlr4_vba.vbaLexer import vbaLexer
-from vba_linter.rules.rule_base import RuleBase
+from vba_linter.rules.token_sequence_base import TokenSequenceBase
 from typing import List, TypeVar
 
 
 T = TypeVar('T', bound='AmbiguousIdentifier')
 
 
-class AmbiguousIdentifier(RuleBase):
+class AmbiguousIdentifier(TokenSequenceBase):
     def __init__(self: T) -> None:
         self._rule_name = "E741"
         self._names = ['l', 'O', 'I']
         self._message = 'ambiguous variable name'
-        self._type = vbaLexer.IDENTIFIER
-
-    def test(self: T, ts: CommonTokenStream) -> list:
-        output: List[tuple] = []
-        token = ts.LT(1)
-        if token.type == self._type:
-            output = self._match_action(token)
-        return output
+        self._sequence = [vbaLexer.IDENTIFIER]
+        self._target = 0
 
     def _match_action(self: T, token: Token) -> list:
         if token.text in self._names:
