765286f04f4ce7861e9a94a22b699b4c566d8f3c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-09 09:00:27 -0500

    Create test_ambiguous_function.py

---
+++ tests/Unit/rules/test_ambiguous_function.py
@@ -0,0 +1,31 @@
+import pytest
+from Unit.rules.rule_test_base import RuleTestBase
+from vba_linter.rules.rule_base import RuleBase
+from vba_linter.rules.ambiguous_identifier import AmbiguousIdentifier
+
+
+anti_patterns = [
+    [
+        RuleTestBase.worst_practice,
+        [(5, 1, 'E741')]
+    ]
+]
+
+
+rule = AmbiguousIdentifier()
+
+
+@pytest.mark.parametrize('rule', [rule])
+@pytest.mark.parametrize(
+    "code, expected",
+    anti_patterns + RuleTestBase.best_practice
+)
+def test_test(rule: RuleBase, code: str, expected: tuple) -> None:
+    assert RuleTestBase.tokenize(rule, code) == expected
+
+
+@pytest.mark.parametrize('rule', [rule])
+def test_message(rule: RuleBase) -> None:
+    data = (4, 1, "E743")
+    expected = ":4:1: E741 ambiguous function name"
+    assert rule.create_message(data) == expected
