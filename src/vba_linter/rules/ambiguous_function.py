93a0510f3e2cd0b61f31b1baf69ff85f3b049412
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-09 11:55:53 -0500

    Update ambiguous_function.py

--- src/vba_linter/rules/ambiguous_function.py
+++ src/vba_linter/rules/ambiguous_function.py
@@ -1,21 +1,16 @@
 from antlr4 import Token
 from antlr4_vba.vbaLexer import vbaLexer
-from vba_linter.rules.token_sequence_base import TokenSequenceBase
+from vba_linter.rules.ambiguous_identifier import AmbiguousIdentifier
 from typing import TypeVar
 
 
 T = TypeVar('T', bound='AmbiguousFunction')
 
 
-class AmbiguousFunction(TokenSequenceBase):
+class AmbiguousFunction(AmbiguousIdentifier):
     def __init__(self: T) -> None:
         self._rule_name = "E743"
         self._names = ['l', 'O', 'I']
         self._message = 'ambiguous function name'
         self._sequence = [vbaLexer.FUNCTION, vbaLexer.WS, vbaLexer.IDENTIFIER]
         self._target = 3
-
-    def _match_action(self: T, token: Token) -> list:
-        if token.text in self._names:
-            return [(token.line, token.column + 1, self._rule_name)]
-        return []
