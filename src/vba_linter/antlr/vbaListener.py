ba795b492556d5d00717655af270918b74634fcb
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-09 18:32:18 -0500

    Update vbaListener.py

--- src/vba_linter/antlr/vbaListener.py
+++ src/vba_linter/antlr/vbaListener.py
@@ -7,5 +7,13 @@ T = TypeVar('T', bound='vbaListener')
 
 
 class vbaListener(ParseTreeListener):
+    def set_token_stream(self: T, ts: CommonTokenStream) -> None:
+        self.ts = ts
+
     def enterLetStmt(self: T, ctx:vbaParser.LetStmtContext):
-        pass
+        tok = self.ts[ctx.eq.index - 1]
+        if tok.type == vbaLexer.WS:
+            if len(tok.text) > 1:
+                raise Exception('too many leading spaces')
+        else:
+            raise Exception('Missing leading space')
