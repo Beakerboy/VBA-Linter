5ee93ffc88d84fab4e4f8939d9194ffd4629a105
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-10 10:43:05 -0500

    Update test_main.py

--- tests/Functional/test_main.py
+++ tests/Functional/test_main.py
@@ -4,6 +4,35 @@ from _pytest.capture import CaptureFixture
 from vba_linter.__main__ import main
 
 
+def test_worst_file(mocker: MockerFixture, capsys: CaptureFixture) -> None:
+    code = RuleTestBase.worst_practice
+    file_name = RuleTestBase.save_code(code)
+    mocker.patch(
+        "sys.argv",
+        [
+            "vba_linter.py",
+            file_name
+        ],
+    )
+    with pytest.raises(SystemExit):
+        main()
+    captured = capsys.readouterr()
+    full_path = "/home/runner/work/VBA-Linter/VBA-Linter/tests/Functional/" + file_name
+    expected = """\
+%s:1:51: E211 whitespace before '('
+%s:1:53: E201 Whitespace after '('
+%s:1:63: E203 Whitespace before ','
+%s:1:80: W501 line too long (91 > 79 characters)
+%s:1:89: E202 Whitespace before ')'
+%s:1:91: W291 trailing whitespace
+%s:1:91: W500 incorrect line ending
+%s:2:0: W500 incorrect line ending
+%s:3:13: W500 incorrect line ending
+%s:4:12: W500 incorrect line ending
+10 Errors in 1 File
+""".replace("%s", full_path)  # noqa
+    assert captured.err == expected
+
 def test_bad_file(mocker: MockerFixture, capsys: CaptureFixture) -> None:
     dir_path = "tests/Files/project"
     mocker.patch(
