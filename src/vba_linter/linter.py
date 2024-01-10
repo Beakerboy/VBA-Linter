4eb8b7e97ec051fc4c0dd922e98276ebba9ce33a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-10 08:21:58 -0500

    Update linter.py

--- src/vba_linter/linter.py
+++ src/vba_linter/linter.py
@@ -49,23 +49,3 @@ class Linter:
 
             output.sort()
         return output
-
-    @classmethod
-    def text_matches(cls: Type[T], pattern: str, name: str) -> bool:
-        match = re.match(pattern, name)
-        if match:
-            return True
-        return False
-
-    @classmethod
-    def is_snake_case(cls: Type[T], name: str) -> bool:
-        pattern = '(^[a-z]{1}$)|([a-z]+(_[a-z]+)*$)'
-        return cls.text_matches(pattern, name)
-
-    @classmethod
-    def is_camel_case(cls: Type[T], name: str) -> bool:
-        """
-        Also known as lowerCamelCase.
-        """
-        pattern = '(^[a-z]{1}$)|([a-z]{2,}([a-zA-Z]([a-z])+)*$)'
-        return cls.text_matches(pattern, name)
