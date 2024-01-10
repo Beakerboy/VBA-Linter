bdd6ab175511315b04c507f08621fb2f54d4144f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-10 08:24:26 -0500

    Update linter.py

--- src/vba_linter/linter.py
+++ src/vba_linter/linter.py
@@ -1,4 +1,3 @@
-import re
 from antlr4 import CommonTokenStream, FileStream, ParseTreeWalker, Token
 from antlr4_vba.vbaLexer import vbaLexer
 from vba_linter.antlr.throwing_error_listener import ThrowingErrorListener
