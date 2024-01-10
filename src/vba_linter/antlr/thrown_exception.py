a680ace5f86831ffb1d023b364c918ef840e92ff
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-10 09:27:34 -0500

    Update thrown_exception.py

--- src/vba_linter/antlr/thrown_exception.py
+++ src/vba_linter/antlr/thrown_exception.py
@@ -1,8 +1,13 @@
 from antlr4.error.Errors import ParseCancellationException
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='ThrownException')
+
 
 class ThrownException(ParseCancellationException):
 
-    def __init__(self, message):
+    def __init__(self: T, message: str):
         super().__init__(message)
         self.msg = ''
         self.line = 0
