375d6a46e3c3827a17b08991dc1cc7ad09010098
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-10 12:03:08 -0500

    Update test_main.py

--- tests/Functional/test_main.py
+++ tests/Functional/test_main.py
@@ -47,7 +47,7 @@ def test_worst_file(mocker: MockerFixture, capsys: CaptureFixture) -> None:
 %s:2:0: W500 incorrect line ending
 %s:4:0: E303 Too many blank lines (3)
 %s:5:11: W500 incorrect line ending
-%s:12:1: W391 blank line at end of file
+%s:8:12: W500 incorrect line ending
 12 Errors in 1 File
 """.replace("%s", full_path)  # noqa
     assert captured.err == expected
