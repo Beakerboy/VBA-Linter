203005c1a9276dfed92ab574d4415efe2b04264b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-10 08:22:29 -0500

    Update vbaListener.py

--- src/vba_linter/antlr/vbaListener.py
+++ src/vba_linter/antlr/vbaListener.py
@@ -3,7 +3,7 @@ from antlr4 import CommonTokenStream, ParseTreeListener
 from antlr4.tree.Tree import TerminalNodeImpl
 from antlr4_vba.vbaLexer import vbaLexer
 from antlr4_vba.vbaParser import vbaParser
-from typing import TypeVar
+from typing import Type, TypeVar
 
 
 T = TypeVar('T', bound='vbaListener')
