9c941e6c56d2a2cd08802799f42a05060259c354
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-10 09:26:14 -0500

    Update throwing_error_listener.py

--- src/vba_linter/antlr/throwing_error_listener.py
+++ src/vba_linter/antlr/throwing_error_listener.py
@@ -1,8 +1,14 @@
 from antlr4.error.ErrorListener import ErrorListener
 from vba_linter.antlr.thrown_exception import ThrownException
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='ThrowingErrorListener')
+
 
 class ThrowingErrorListener(ErrorListener):
-    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+    def syntaxError(self: T, recognizer, offendingSymbol, line: int,
+                    column: int, msg: str, e):
         ex = ThrownException(f'line {line}: {column} {msg}')
         ex.line = line
         ex.column = column
