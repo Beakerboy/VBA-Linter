1764be60b4537997c31e6dc7d46f229f1f5e19bc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-10 10:17:49 -0500

    Update throwing_error_listener.py

--- src/vba_linter/antlr/throwing_error_listener.py
+++ src/vba_linter/antlr/throwing_error_listener.py
@@ -7,7 +7,7 @@ T = TypeVar('T', bound='ThrowingErrorListener')
 
 
 class ThrowingErrorListener(ErrorListener):
-    def syntaxError(self: T, recognizer,  # noqa: N802
+    def syntaxError(self: T, recognizer: Any,  # noqa: N802
                     offending_symbol: Any, line: int,
                     column: int, msg: str, e: Exception) -> None:
         ex = ThrownException(f'line {line}: {column} {msg}')
