dae74cece4042c0966ef4e531a5310738226ade4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-10 08:33:03 -0500

    Update rule_test_base.py

--- tests/Unit/rules/rule_test_base.py
+++ tests/Unit/rules/rule_test_base.py
@@ -31,8 +31,8 @@ class RuleTestBase:
         '\r\n' +
         '\r\n' +
         'I  =  (2+1)\n' +
-        '    foo_val=6\n'
-        '    Let BarVal  =  7\n'
+        '    foo_val=6\r\n'
+        '    Let BarVal  =  7\r\n'
         'End Function\n' +
         '\r\n' +
         'Public Function O()\r\n' +
