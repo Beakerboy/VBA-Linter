3563dd16728881645b0516e75b0e5566f10bd96e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-11 20:55:08 -0500

    Update test_main.py

--- tests/Functional/test_main.py
+++ tests/Functional/test_main.py
@@ -82,53 +82,6 @@ def test_worst_file(mocker: MockerFixture, capsys: CaptureFixture) -> None:
     delete_code(file_name + ".pretty")
 
 
-def test_bad_file(mocker: MockerFixture, capsys: CaptureFixture) -> None:
-    dir_path = "tests/Files/project"
-    mocker.patch(
-        "sys.argv",
-        [
-            "vba_linter.py",
-            dir_path
-        ],
-    )
-    with pytest.raises(SystemExit):
-        main()
-    captured = capsys.readouterr()
-    expected = """\
-/home/runner/work/VBA-Linter/VBA-Linter/tests/Files/project/all_errors.bas:1:51: E211 whitespace before '('
-/home/runner/work/VBA-Linter/VBA-Linter/tests/Files/project/all_errors.bas:1:53: E201 Whitespace after '('
-/home/runner/work/VBA-Linter/VBA-Linter/tests/Files/project/all_errors.bas:1:63: E203 Whitespace before ','
-/home/runner/work/VBA-Linter/VBA-Linter/tests/Files/project/all_errors.bas:1:80: W501 line too long (91 > 79 characters)
-/home/runner/work/VBA-Linter/VBA-Linter/tests/Files/project/all_errors.bas:1:89: E202 Whitespace before ')'
-/home/runner/work/VBA-Linter/VBA-Linter/tests/Files/project/all_errors.bas:1:91: W291 trailing whitespace
-/home/runner/work/VBA-Linter/VBA-Linter/tests/Files/project/all_errors.bas:1:91: W500 incorrect line ending
-/home/runner/work/VBA-Linter/VBA-Linter/tests/Files/project/all_errors.bas:2:0: W500 incorrect line ending
-/home/runner/work/VBA-Linter/VBA-Linter/tests/Files/project/all_errors.bas:3:13: W500 incorrect line ending
-/home/runner/work/VBA-Linter/VBA-Linter/tests/Files/project/all_errors.bas:4:12: W500 incorrect line ending
-10 Errors in 1 File
-"""  # noqa
-    assert captured.err == expected
-
-
-def test_fail_file(mocker: MockerFixture, capsys: CaptureFixture) -> None:
-    dir_path = "tests/Files/Fail"
-    mocker.patch(
-        "sys.argv",
-        [
-            "vba_linter.py",
-            dir_path
-        ],
-    )
-    with pytest.raises(SystemExit):
-        main()
-    captured = capsys.readouterr()
-    expected = """\
-/home/runner/work/VBA-Linter/VBA-Linter/tests/Files/Fail/fail.bas:3:14: E999 extraneous input '+1' expecting {',', ')', WS}
-1 Error in 1 File
-"""  # noqa
-    assert captured.err == expected
-
-
 def save_code(code: str) -> str:
     file_name = create_filename(ext='.bas')
     p = Path(file_name)
