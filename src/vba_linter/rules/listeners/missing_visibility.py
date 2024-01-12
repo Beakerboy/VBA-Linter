68bd05eb1b69330dd1fab7c1371de843e1ad8a16
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-11 21:32:31 -0500

    Update missing_visibility.py

--- src/vba_linter/rules/listeners/missing_visibility.py
+++ src/vba_linter/rules/listeners/missing_visibility.py
@@ -2,29 +2,25 @@ from antlr4 import CommonTokenStream, ParseTreeListener, ParserRuleContext
 from antlr4.tree.Tree import TerminalNodeImpl
 from antlr4_vba.vbaLexer import vbaLexer
 from antlr4_vba.vbaParser import vbaParser
-from typing import Type, TypeVar
-from vba_linter.antlr.vbaListener import VbaListener
+from typing import TypeVar
 
 
 T = TypeVar('T', bound='MissingVisibility')
 
 
 class MissingVisibility(ParseTreeListener):
-    def __init__(self: T) -> None:
-        super().__init__()
-        self.output: list = []
+    def enterFunctionStmt(self: T,  # noqa: N802
+                          ctx: vbaParser.FunctionStmtContext) -> None:
+        self.enter_function_sub_stmt(ctx)
 
-    def set_token_stream(self: T, ts: CommonTokenStream) -> None:
-        self.ts = ts
+    def enterSubStmt(self: T,  # noqa: N802
+                     ctx: vbaParser.SubStmtContext) -> None:
+        self.enter_function_sub_stmt(ctx)
 
     def enter_function_sub_stmt(self: T, ctx: ParserRuleContext) -> None:
         child = ctx.getChild(0)
         tok = ctx.start
-        if isinstance(child, vbaParser.VisibilityContext):
-            if tok.text == "Public":
-                self.output.append((tok.line, tok.column + 1,
-                                    "Wxxx", "optional public"))
-        else:
+        if not isinstance(child, vbaParser.VisibilityContext):
             line = tok.line
             column = tok.column
             msg = "missing visibility"
