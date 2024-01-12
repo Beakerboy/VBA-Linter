c50aff0f9e507a0ce8282d3e98442561deddc60c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-11 22:18:54 -0500

    Update optional_public.py

--- src/vba_linter/rules/listeners/optional_public.py
+++ src/vba_linter/rules/listeners/optional_public.py
@@ -1,13 +1,16 @@
 from antlr4 import ParseTreeListener, ParserRuleContext
 from antlr4_vba.vbaParser import vbaParser
 from typing import TypeVar
-from vba_linter.antlr.vbaListener import VbaListener
 
 
 T = TypeVar('T', bound='OptionalPublic')
 
 
 class OptionalPublic(ParseTreeListener):
+    def __init__(self: T) -> None:
+        super().__init__()
+        self.output: list = []
+
     def enterFunctionStmt(self: T,  # noqa: N802
                           ctx: vbaParser.FunctionStmtContext) -> None:
         self.enter_function_sub_stmt(ctx)
