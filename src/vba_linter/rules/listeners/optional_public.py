f48ddbcf3321bcf35d6bf52fd3390ce6c60779ca
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-11 21:23:26 -0500

    Create optional_public.py

---
+++ src/vba_linter/rules/listeners/optional_public.py
@@ -0,0 +1,26 @@
+from antlr4 import CommonTokenStream, ParseTreeListener, ParserRuleContext
+from antlr4.tree.Tree import TerminalNodeImpl
+from antlr4_vba.vbaLexer import vbaLexer
+from antlr4_vba.vbaParser import vbaParser
+from typing import Type, TypeVar
+from vba_linter.antlr.vbaListener import VbaListener
+
+
+T = TypeVar('T', bound='OptionalPublic')
+
+
+class OptionalPublic(ParseTreeListener):
+    def __init__(self: T) -> None:
+        super().__init__()
+        self.output: list = []
+
+    def set_token_stream(self: T, ts: CommonTokenStream) -> None:
+        self.ts = ts
+
+    def enter_function_sub_stmt(self: T, ctx: ParserRuleContext) -> None:
+        child = ctx.getChild(0)
+        tok = ctx.start
+        if isinstance(child, vbaParser.VisibilityContext):
+            if tok.text == "Public":
+                self.output.append((tok.line, tok.column + 1,
+                                    "Wxxx", "optional public"))
