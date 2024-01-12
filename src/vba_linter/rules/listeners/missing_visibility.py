f68b6182f7ab0ec5665f4068505748e593939829
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-11 22:25:01 -0500

    Update missing_visibility.py

--- src/vba_linter/rules/listeners/missing_visibility.py
+++ src/vba_linter/rules/listeners/missing_visibility.py
@@ -7,6 +7,10 @@ T = TypeVar('T', bound='MissingVisibility')
 
 
 class MissingVisibility(ParseTreeListener):
+    def __init__(self: T) -> None:
+        super().__init__()
+        self.output: list = []
+
     def enterFunctionStmt(self: T,  # noqa: N802
                           ctx: vbaParser.FunctionStmtContext) -> None:
         self.enter_function_sub_stmt(ctx)
