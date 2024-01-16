eae21774cafb3688f6df217b60213fb1ec6527f8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-16 13:19:15 -0500

    Update rule_directory.py

--- src/vba_linter/rule_directory.py
+++ src/vba_linter/rule_directory.py
@@ -6,6 +6,7 @@ from vba_linter.rules.mixed_indent import MixedIndent
 from vba_linter.rules.trailing_whitespace import TrailingWhitespace
 from vba_linter.rules.newline_eof import NewlineEof
 from vba_linter.rules.token_sequence_base import TokenSequenceBase
+from vba_linter.rules.token_sequence_mismatch import TokenSequenceMismatch
 from vba_linter.rules.blank_line_eof import BlankLineEof
 from vba_linter.rules.line_ending import LineEnding
 from vba_linter.rules.line_too_long import LineTooLong
