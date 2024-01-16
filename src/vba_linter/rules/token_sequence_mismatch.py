3a33293b98ea82e5148707420d79931823ecba61
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-16 13:14:37 -0500

    Update token_sequence_mismatch.py

--- src/vba_linter/rules/token_sequence_mismatch.py
+++ src/vba_linter/rules/token_sequence_mismatch.py
@@ -1,6 +1,6 @@
-from antlr4 import CommonTokenStream, Token
+from antlr4 import CommonTokenStream
 from vba_linter.rules.rule_base import RuleBase
-from typing import List, Tuple, Type, TypeVar, Union
+from typing import TypeVar
 
 
 T = TypeVar('T', bound='TokenSequenceMismatch')
@@ -11,7 +11,7 @@ class TokenSequenceMismatch(RuleBase):
     Create an error if the stream mathes a given sequence
     of token types.
     """
-    
+
     def match(self: T, sequence: list, signature: list) -> bool:
         """
         Compare the two lists to see if they match.
