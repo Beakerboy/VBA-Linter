9c5168baf022ed1df1631b2dc6fbc15b899d5d29
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-17 17:39:05 -0500

    Update rule_directory.py

--- src/vba_linter/rule_directory.py
+++ src/vba_linter/rule_directory.py
@@ -59,8 +59,7 @@ class RuleDirectory:
         e203 = TokenSequenceBase("E203",
                                  [vbaLexer.WS, vbaLexer.T__0], 0,
                                  "Whitespace before ','")
-        self._rules.update({"E201": e201, "E202": e202, "E203": e203})
-        self._rules.update({"E201": e201, "E202": e202, "E203": e203})
+        self._rules.update({"E202": e202, "E203": e203})
         self._rules.update({"W291": TrailingWhitespace(), "W201": NewlineEof(),
                             "W391": BlankLineEof(), "W500": LineEnding(),
                             "W501": LineTooLong(), "E101": MixedIndent()})
