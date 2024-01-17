4e01a0a81980152512195ac37e8b84e38ec14672
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-17 13:20:00 -0500

    Create test_token_sequence_base_tuple.py

---
+++ tests/Unit/rules/test_token_sequence_base_tuple.py
@@ -0,0 +1,24 @@
+import pytest
+from antlr4_vba.vbaLexer import vbaLexer
+from Unit.rules.rule_test_base import RuleTestBase
+from vba_linter.rules.token_sequence_base import ToeknSequenceBase
+
+
+anti_patterns = [
+    [
+        'Public Function Foo (num, bar)\r\nEnd Function\r\n",
+        [(2, 20, "E203")]
+    ]
+]
+
+
+rule = TokenSequenceBase("F00", ([vbaLexer.WS], [vbaLexer.LPAREN]), 1, )
+
+
+@pytest.mark.parametrize('rule', [rule])
+@pytest.mark.parametrize(
+    "code, expected",
+    anti_patterns + RuleTestBase.best_practice
+)
+def test_test(rule: RuleBase, code: str, expected: tuple) -> None:
+    assert RuleTestBase.tokenize(rule, code) == expected
