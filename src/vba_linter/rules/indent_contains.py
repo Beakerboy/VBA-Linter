1e54425b37d594ab84471711767bd8899259fd65
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-17 11:49:49 -0500

    Update indent_contains.py

--- src/vba_linter/rules/indent_contains.py
+++ src/vba_linter/rules/indent_contains.py
@@ -20,6 +20,7 @@ class IndentContains(RuleBase):
     def test(self: T, ts: CommonTokenStream) -> list:
         output: List[tuple] = []
         token = ts.LT(1)
+        assert token is not None
         if token.type == vbaLexer.WS and token.column == 0:
             # if next token exists and is not NEWLINE
             # should the scope be checked to decide if this
