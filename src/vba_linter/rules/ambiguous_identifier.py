15c53010179114e5bfd4d9af174abaaa5dca1e91
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-17 22:06:09 -0500

    Update ambiguous_identifier.py

--- src/vba_linter/rules/ambiguous_identifier.py
+++ src/vba_linter/rules/ambiguous_identifier.py
@@ -9,17 +9,17 @@ T = TypeVar('T', bound='AmbiguousIdentifier')
 
 class AmbiguousIdentifier(TokenSequenceBase):
     def __init__(self: T) -> None:
-        self._severity = 'E'
-        self._rule_name = "741"
-        self._names = ['l', 'O', 'I']
-        self._message = 'ambiguous variable name'
-        self._sequence = (
+        rule_name = "741"
+        self._bad_names = ['l', 'O', 'I']
+        message = 'ambiguous variable name'
+        sequence = (
             [vbaLexer.IDENTIFIER, vbaLexer.WS, vbaLexer.AS],
             [vbaLexer.IDENTIFIER, vbaLexer.WS, vbaLexer.EQ]
         )
-        self._target = 1
+        target = 1
+        super().__init__(rule_name, sequence, target, message)
 
     def _match_action(self: T, token: Token) -> list:
-        if token.text in self._names:
+        if token.text in self._bad_names:
             return [(token.line, token.column + 1, self._rule_name)]
         return []
