103c25c246026c8f69251a731920239768f8b107
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-18 10:22:52 -0500

    Create parameter_unused.py

---
+++ src/vba_linter/rules/listeners/parameter_unused.py
@@ -0,0 +1,31 @@
+from antlr4 import CommonTokenStream, ParseTreeListener, ParserRuleContext
+from antlr4_vba.vbaLexer import vbaLexer
+from antlr4_vba.vbaParser import vbaParser
+from typing import TypeVar
+from vba_linter.antlr.vbaListener import VbaListener
+
+
+T = TypeVar('T', bound='ParameterUnused')
+
+
+class ParameterUnused(ParseTreeListener):
+    def __init__(self: T) -> None:
+        super().__init__()
+        self.output: list = []
+
+    def set_token_stream(self: T, ts: CommonTokenStream) -> None:
+        self.ts = ts
+
+    def enterFunctionStmt(self: T,  # noqa: N802
+                          ctx: vbaParser.FunctionStmtContext) -> None:
+        self.enter_function_sub_stmt(ctx)
+
+    def enterSubStmt(self: T,  # noqa: N802
+                     ctx: vbaParser.SubStmtContext) -> None:
+        self.enter_function_sub_stmt(ctx)
+
+    def enter_function_sub_stmt(self: T, ctx: ParserRuleContext) -> None:
+        # go to the arglist context to get the parameters
+        # check off any that are used in procedure calls
+        # check off any that are used in a ValueStmt.
+              
