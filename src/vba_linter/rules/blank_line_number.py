bf124f292861959b183be1be17362e86d4a6f4ea
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-21 09:10:28 -0500

    Create blank_line_number.py

---
+++ src/vba_linter/rules/blank_line_number.py
@@ -0,0 +1,34 @@
+from antlr4 import CommonTokenStream
+from antlr4_vba.vbaLexer import vbaLexer
+from vba_linter.rules.rule_base import RuleBase
+from typing import List, TypeVar
+
+
+T = TypeVar('T', bound='BlankLineNumber')
+
+
+class BlankLineNumber(RuleBase):
+    def __init__(self: T) -> None:
+        super().__init__()
+        self._severity = 'W'
+        self._rule_name = "303"
+        self._line_ending = '\r\n'
+        self._allowed_blank_lines = 2
+        self._message = 'Too many blank lines (3)'
+        self._fixable = True
+
+    def test(self: T, ts: CommonTokenStream) -> list:
+        name = self._rule_name
+        output: List[tuple] = []
+        token = ts.LT(1)
+        assert token is not None
+        if token.type == vbaLexer.NEWLINE:
+            newline_list = RuleBase.split_nl(token.text)
+            num_nl = len(newline_list)
+            for i in range(num_nl):
+                if i > self._allowed_blank_lines:
+                    output.append((token.line + i, -1, name))
+            num = min([num_nl, self._allowed_blank_lines + 1])
+            new_text = self._line_ending * num
+            token.text = new_text
+        return output
