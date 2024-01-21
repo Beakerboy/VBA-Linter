a32c9ad0260fb03439b50176116ae8f165a79dba
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-21 17:34:25 -0500

    Update test_rule_directory.py

--- tests/Unit/test_rule_directory.py
+++ tests/Unit/test_rule_directory.py
@@ -1,4 +1,5 @@
 from vba_linter.rule_directory import RuleDirectory
+from Unit.parser_rule_stub import ParserRuleStub
 from Unit.rule_stub import RuleStub
 
 
@@ -10,9 +11,15 @@ def test_constructor() -> None:
 def test_add_and_remove() -> None:
     obj = RuleDirectory()
     rule1 = RuleStub()
+    rule2 = ParserRuleStub("002")
     rule1.set_name("001")
     assert len(obj.get_loaded_rules()) == 0
     obj.add_rule(rule1)
     assert len(obj.get_loaded_rules()) == 1
     obj.remove_rule("001")
     assert len(obj.get_loaded_rules()) == 0
+    obj.add_rule(rule2)
+    assert len(obj.get_loaded_rules()) == 0
+    assert len(obj.get_parser_rules()) == 1
+    obj.remove_rule("002")
+    assert len(obj.get_parser_rules()) == 0
