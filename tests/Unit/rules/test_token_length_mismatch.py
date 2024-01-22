5803871efc15d68cfbe7c67df43b7de3cba3dca4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-21 19:18:38 -0500

    Update test_token_length_mismatch.py

--- tests/Unit/rules/test_token_length_mismatch.py
+++ tests/Unit/rules/test_token_length_mismatch.py
@@ -8,7 +8,8 @@ from vba_linter.rules.token_length_mismatch import TokenLengthMismatch
 anti_patterns = [
     [
         RuleTestBase.worst_practice,
-        [(7, 16, "E303")]
+        [(5,3,"303"),
+         (7, 16, "303")]
     ]
 ]
 
