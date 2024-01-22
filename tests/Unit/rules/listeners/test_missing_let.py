3258db16d85072980e445ddd93907f68da426250
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-22 08:44:02 -0500

    Update test_missing_let.py

--- tests/Unit/rules/listeners/test_missing_let.py
+++ tests/Unit/rules/listeners/test_missing_let.py
@@ -1,7 +1,24 @@
-from antlr4 import CommonTokenStream, ParseTreeWalker
+import pytest
+from Unit.rules.rule_test_base import RuleTestBase
+from vba_linter.rules.rule_base import RuleBase
 from vba_linter.rules.listeners.missing_let import MissingLet
 
-listener = MissingLet()
-listener.set_token_stream(ts1)
-ParseTreeWalker.DEFAULT.walk(rule, program)
-listener.get_output()
+
+anti_patterns = [
+    [
+        RuleTestBase.worst_practice,
+        [(10, 17, '743')]
+    ]
+]
+
+
+rule = MissingLet()
+
+
+@pytest.mark.parametrize('rule', [rule])
+@pytest.mark.parametrize(
+    "code, expected",
+    anti_patterns + RuleTestBase.best_practice
+)
+def test_test(rule: RuleBase, code: str, expected: tuple) -> None:
+    assert RuleTestBase.run_test(rule, code) == expected
