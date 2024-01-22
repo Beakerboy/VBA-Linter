09d3cae12c8fffb905392f98fc20d39b42c16a32
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-21 22:25:49 -0500

    Update rule_test_base.py

--- tests/Unit/rules/rule_test_base.py
+++ tests/Unit/rules/rule_test_base.py
@@ -66,12 +66,12 @@ class RuleTestBase:
     def run_token_test(cls: Type[T], rule: RuleBase, code: str) -> list:
         file_name = cls.save_code(code)
         ts = cls.create_tokens(file_name)
-        results = cls.run_test(rule, ts)
+        results = cls.run_rule(rule, ts)
         cls.delete_code(file_name)
         return results
 
     @classmethod
-    def run_test(cls: Type[T], rule: RuleBase, ts: CommonTokenStream) -> list:
+    def run_rule(cls: Type[T], rule: RuleBase, ts: CommonTokenStream) -> list:
         """
         Walk the tokenstream, testing each token against the rule.
         """
