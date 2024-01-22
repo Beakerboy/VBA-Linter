52fbaa571203e31c0daa1941e90c3f1b0d533c85
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-22 16:02:44 -0500

    Update test_token_sequence_base_list.py

--- tests/Unit/rules/test_token_sequence_base_list.py
+++ tests/Unit/rules/test_token_sequence_base_list.py
@@ -28,4 +28,4 @@ rule = TokenSequenceBase("E202",
     anti_patterns + RuleTestBase.best_practice
 )
 def test_test(rule: RuleBase, code: str, expected: tuple) -> None:
-    assert RuleTestBase.tokenize(rule, code) == expected
+    assert RuleTestBase.run_test(rule, code) == expected
