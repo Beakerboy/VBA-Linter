078aa179cfd43b6b49918fdaa5aad5ce53301dc1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-21 19:25:57 -0500

    Create test_token_seq_operator.py

---
+++ tests/Unit/rules/test_token_seq_operator.py
@@ -0,0 +1,30 @@
+import pytest
+from antlr4_vba.vbaLexer import vbaLexer
+from Unit.rules.rule_test_base import RuleTestBase
+from vba_linter.rules.rule_base import RuleBase
+from vba_linter.rules.token_seq_operator import TokenSequenceOperator
+
+
+anti_patterns = [
+    [
+        RuleTestBase.worst_practice,
+        [(5, 3, "303"),
+         (7, 16, "303")]
+    ]
+]
+
+
+rule = TokenSequenceOperator(
+    "303",
+    [vbaLexer.WS, vbaLexer.EQ], 0,
+    "Excess whitespace before '='"
+)
+
+
+@pytest.mark.parametrize('rule', [rule])
+@pytest.mark.parametrize(
+    "code, expected",
+    anti_patterns + RuleTestBase.best_practice
+)
+def test_test(rule: RuleBase, code: str, expected: tuple) -> None:
+    assert RuleTestBase.tokenize(rule, code) == expected
