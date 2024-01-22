ced1e5d896cc2132a90496864470c977f908e554
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-22 15:56:11 -0500

    Update test_indent_contains.py

--- tests/Unit/rules/test_indent_contains.py
+++ tests/Unit/rules/test_indent_contains.py
@@ -30,7 +30,7 @@ rule = IndentContains()
     anti_patterns + RuleTestBase.best_practice
 )
 def test_test(rule: RuleBase, code: str, expected: tuple) -> None:
-    assert RuleTestBase.tokenize(rule, code) == expected
+    assert RuleTestBase.run_test(rule, code) == expected
 
 
 @pytest.mark.parametrize('rule', [rule])
