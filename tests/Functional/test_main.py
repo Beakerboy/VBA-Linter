045e9d642a8345bf045c6155d835eff9a500e9f4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-23 12:51:53 -0500

    Update test_main.py

--- tests/Functional/test_main.py
+++ tests/Functional/test_main.py
@@ -159,7 +159,6 @@ def test_worst_file_std(mocker: MockerFixture, capsys: CaptureFixture) -> None:
         main()
     captured = capsys.readouterr()
     assert captured.err == expected
-    delete_code(file_name)
 
 
 def test_worst_silent(mocker: MockerFixture, capsys: CaptureFixture) -> None:
