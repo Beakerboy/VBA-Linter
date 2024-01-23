8d8f7cfe91b6161d472f8e0a80acf5120b629050
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-23 08:24:23 -0500

    Create test_missing_visibility.py

---
+++ tests/Unit/rules/listeners/test_missing_visibility.py
@@ -0,0 +1,24 @@
+import pytest
+from Unit.rules.rule_test_base import RuleTestBase
+from vba_linter.rules.rule_base import RuleBase
+from vba_linter.rules.listeners.missing_visibility import (
+    MissingVisibility)
+
+
+anti_patterns = [
+    [
+        RuleTestBase.worst_practice,
+        [(10, 1, '510')]
+    ]
+]
+
+
+rule = MissingVisibility()
+
+
+@pytest.mark.parametrize('rule', [rule])
+@pytest.mark.parametrize(
+    "code, expected", anti_patterns + RuleTestBase.best_practice
+)
+def test_test(rule: RuleBase, code: str, expected: tuple) -> None:
+    assert RuleTestBase.run_test(rule, code) == expected
