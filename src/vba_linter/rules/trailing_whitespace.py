ce37169be8201dd3da4acb47ec77a22d39a3edf5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-24 06:20:18 -0500

    Update trailing_whitespace.py

--- src/vba_linter/rules/trailing_whitespace.py
+++ src/vba_linter/rules/trailing_whitespace.py
@@ -8,18 +8,10 @@ T = TypeVar('T', bound='TrailingWhitespace')
 
 class TrailingWhitespace(TokenSequenceMismatch):
     def __init__(self: T) -> None:
-        message = "trailing whitespace"
+        message = "Trailing whitespace"
         super().__init__(
             "305",
             [vbaLexer.NEWLINE, vbaLexer.WS, vbaLexer.NEWLINE],
             1,
             message
         )
-
-    def create_message(self: T, data: tuple) -> str:
-        if data[1] == 1:
-            data = (data[0], 1, "310")
-            self._message = "Blank line contains whitespace"
-        else:
-            self._message = "trailing whitespace"
-        return super().create_message(data)
