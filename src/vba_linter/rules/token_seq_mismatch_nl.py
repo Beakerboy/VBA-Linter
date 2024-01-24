5a762dc3798141bbeefd4f57633e185eb466eeaf
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-24 10:01:47 -0500

    Update token_seq_mismatch_nl.py

--- src/vba_linter/rules/token_seq_mismatch_nl.py
+++ src/vba_linter/rules/token_seq_mismatch_nl.py
@@ -12,14 +12,6 @@ class TokenSeqMismatchNL(TokenSequenceMismatch):
     create an error is a seqenece matches all tokens,
     but mist be not equal at the specified position.
     """
-    def _match_action(self: T, token: Token) -> list:
-        line = token.line
-        if self._target == 1:
-            column = token.column + len(token.text)
-        elif self._target == 2:
-            column = token.column
-        name = self._rule_name
-        return [(line, column + 1, name)]
 
     def match(self: T, sequence: list, signature: list) -> bool:
         """
@@ -27,13 +19,5 @@ class TokenSeqMismatchNL(TokenSequenceMismatch):
         sequence is the list of tokens from the stream
         signature is the 'target'
         """
-        result = True
-        for i in range(len(sequence)):
-            if i == self._target - 1:
-                result = result and (
-                    sequence[i] != signature[i] or
-                    sequence[i] == vbaLexer.NEWLINE
-                )
-            else:
-                result = result and sequence[i] == signature[i]
-        return result
+        result = super().match(sequence, signature)
+        return result and sequence[1] != vbaLexer.NEWLINE
