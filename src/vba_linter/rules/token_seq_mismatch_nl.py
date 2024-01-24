df8da536b40240ea82a43168cc5e686009c93ee0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-24 09:47:50 -0500

    Create token_seq_mismatch_nl.py

---
+++ src/vba_linter/rules/token_seq_mismatch_nl.py
@@ -0,0 +1,38 @@
+from antlr4 import Token
+from vba_linter.rules.token_sequence_mismatch import TokenSequenceMismatch
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='TokenSeqMismatchNL')
+
+
+class TokenSeqMismatchNL(TokenSequenceMismatch):
+    """
+    create an error is a seqenece matches all tokens,
+    but mist be not equal at the specified position.
+    """
+    def _match_action(self: T, token: Token) -> list:
+        line = token.line
+        if self._target == 1:
+            column = token.column + len(token.text)
+        elif self._target == 2:
+            column = token.column
+        name = self._rule_name
+        return [(line, column + 1, name)]
+
+    def match(self: T, sequence: list, signature: list) -> bool:
+        """
+        Compare the two lists to see if they match.
+        sequence is the list of tokens from the stream
+        signature is the 'target'
+        """
+        result = True
+        for i in range(len(sequence)):
+            if i == self._target - 1:
+                result = result and (
+                    sequence[i] != signature[i] or 
+                    sequence[i] == vbaLexer.NEWLINE
+                )
+            else:
+                result = result and sequence[i] == signature[i]
+        return result
