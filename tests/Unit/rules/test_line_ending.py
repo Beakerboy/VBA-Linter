7a3b821b1701b29c7b9a42276c98db3bab3c5798
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-13 09:59:06 -0500

    Update test_line_ending.py

--- tests/Unit/rules/test_line_ending.py
+++ tests/Unit/rules/test_line_ending.py
@@ -5,24 +5,13 @@ from vba_linter.rules.line_ending import LineEnding
 
 
 anti_patterns = [
-    ('\n\r\nFunction Foo()\r\n\r\nEnd Function\r\n', [(1, 0, "500")]),
-    ('\r\n\nFunction Foo()\r\n\r\nEnd Function\r\n', [(2, 0, "500")]),
-    ('\r\n\r\nFoo\n', [(3, 3, "500")]),
-    (
-        'Public Function Foo(num)\r\nEnd Function\n',
-        [(2, 12, "500")]
-    ),
-    (
-        'Public Function Foo(num)\nEnd Function\n',
-        [(1, 24, "500"), (2, 12, "500")]
-    ),
     (
         RuleTestBase.worst_practice,
         [
-            (1, 92, "500"),
-            (2, 0, "500"),
+            (2, 92, "500"),
+            (3, 0, "500"),
             (5, 11, "500"),
-            (8, 12, "500")
+            (9, 12, "500")
         ]
     )
 ]
