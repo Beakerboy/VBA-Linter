386c19ba57c937781d980cfa315ee36e5001cf50
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-24 10:36:22 -0500

    Update test_form.py

--- tests/Functional/test_form.py
+++ tests/Functional/test_form.py
@@ -1,8 +1,13 @@
 from pytest_mock import MockerFixture
 from vba_linter.__main__ import main
+from vba_linter.linter import Linter
+from vba_linter.rule_directory import RuleDirectory
 
 
 def test_form(mocker: MockerFixture) -> None:
+    dir = RuleDirectory()
+    dir.load_standard_rules()
+    results = linter.lint(dir, file_name)
     mocker.patch(
         "sys.argv",
         [
