302759a62a894a1e019219b9fcf209e7d263d88e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-24 14:04:56 -0500

    Update test_form.py

--- tests/Unit/test_form.py
+++ tests/Unit/test_form.py
@@ -15,6 +15,6 @@ def test_form(mocker: MockerFixture) -> None:
     ignored_lines = rule_disabler.ignored
     assert "151" in ignored_lines
     assert "154" in ignored_lines
-    for line_num in range(0, 7):
+    for line_num in range(3, 9):
         assert line_num in ignored_lines["151"]
         assert line_num in ignored_lines["154"]
