ab679d7d925f9bd8e09c0da15c1c83170dbaa3b9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-24 08:16:20 -0500

    Update test_form.py

--- tests/Functional/test_form.py
+++ tests/Functional/test_form.py
@@ -1,3 +1,7 @@
+from pytest_mock import MockerFixture
+from vba_linter.__main__ import main
+
+
 def test_form(mocker: MockerFixture, capsys: CaptureFixture) -> None:
     file_name = save_code(worst_practice)
     files.append(file_name)
