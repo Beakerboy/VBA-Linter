5d0991646d0650982ed96c18fdd62ea3893e91b2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-25 09:07:10 -0500

    Update test_form.py

--- tests/Unit/test_form.py
+++ tests/Unit/test_form.py
@@ -10,11 +10,10 @@ def test_form(mocker: MockerFixture) -> None:
     dir.load_standard_rules()
     dir.remove_rule("400")
     results = linter.lint(dir, file_name)
-    assert len(results) == 13
+    assert len(results) == 7
     rule_disabler = dir.get_rule_disabler()
     ignored_lines = rule_disabler.ignored
     assert "151" in ignored_lines
     assert "154" in ignored_lines
     for line_num in range(3, 9):
-        assert line_num in ignored_lines["151"]
         assert line_num in ignored_lines["154"]
