63f92530735630604ef1e64e67f4c12960b0b621
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-26 21:59:50 -0500

    Update linter.py

--- src/vba_linter/linter.py
+++ src/vba_linter/linter.py
@@ -19,7 +19,7 @@ class Linter:
 
     def get_lexer(self: T, file: str) -> vbaLexer:
         if Path(file).exists():
-            input_stream = FileStream(file, 'utf-16')
+            input_stream = FileStream(file, 'utf-8')
             return vbaLexer(input_stream)
         raise Exception('file does not exist')
 
