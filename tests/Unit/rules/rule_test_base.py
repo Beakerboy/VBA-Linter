8f152d3073927bfadaf304dbcb7e01c33547edd1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-03-03 16:05:03 -0500

    Update rule_test_base.py

--- tests/Unit/rules/rule_test_base.py
+++ tests/Unit/rules/rule_test_base.py
@@ -23,7 +23,7 @@ class RuleTestBase:
              '    bar = data(1)\r\n' +
              '    baz = (2 + 1)\r\n' +
              '    Dim Foo()    As String\r\n' +
-             '    Bar(2, ,"three")\r\n' +
+             '    Bar(2, , baz)\r\n' +
              '    x = 3:    y = 4\r\n' +
              'End Function\r\n'),
             []
