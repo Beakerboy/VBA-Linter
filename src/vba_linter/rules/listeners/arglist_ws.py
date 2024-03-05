ba69771e36f909a9b4b54703646b7f82faeb4251
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-03-05 15:53:56 -0500

    Create arglist_ws.py

---
+++ src/vba_linter/rules/listeners/arglist_ws.py
@@ -0,0 +1,32 @@
+from antlr4 import ParseTreeListener, ParserRuleContext
+from antlr4_vba.vbaLexer import vbaLexer
+from antlr4_vba.vbaParser import vbaParser
+from typing import TypeVar
+from vba_linter.antlr.vbaListener import VbaListener
+
+
+T = TypeVar('T', bound='ArglistWs')
+
+
+class ArglistWs(ParseTreeListener):
+    """
+    Inspect the whitespace before an argument list to
+    ensure there is no whitespace between the function
+    name and the left parenthesis.
+    """
+
+    def enterArgumentList(self: T,  # noqa: N802
+                          ctx: vbaParser.ArgumentListContext) -> None:
+        """
+        foo = bar(1, 2, 3)
+        Call bar(1, 2, 3)
+        """
+        token = ctx.start
+        if token.LT(-1).symbol == vbaLexer.LPAREN:
+            ws = token.LT(-2)
+            if ws.symbol == vbaLexer.WS:
+                self.output.append((ws.line, ws.column, "221"))
+        elif token.LT(-2).symbol == vbaLexer.LPAREN:
+            ws = token.LT(-3)
+            if ws.symbol == vbaLexer.WS:
+                self.output.append((ws.line, ws.column, "221"))
