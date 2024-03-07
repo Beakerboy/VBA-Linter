3688ca6db39a05f7d9a0ccfd7c178c35bb409794
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-03-07 06:18:50 -0500

    Update test_args_ws.py

--- tests/Unit/rules/listeners/test_args_ws.py
+++ tests/Unit/rules/listeners/test_args_ws.py
@@ -7,7 +7,7 @@ from vba_linter.rules.listeners.arglist_ws import ArglistWs
 anti_patterns = [
     [
         RuleTestBase.worst_practice,
-        [(2, 20, '221'), (8, 23, '221')]
+        [(2, 51, '221'), (8, 23, '221')]
     ]
 ]
 
