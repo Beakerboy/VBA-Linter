e300dfdb296874bd6ebd8f42941ddf37f13650ac
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-03-08 17:10:23 -0500

    Update test_e999.py

--- tests/Unit/rules/test_e999.py
+++ tests/Unit/rules/test_e999.py
@@ -4,7 +4,8 @@ from vba_linter.rules.parsing_error import ParsingError
 
 
 txt = ("mismatched input '<' expecting " +
-       "{'ATTRIBUTE', 'VERSION', LINE_CONTINUATION, NEWLINE, REMCOMMENT, COMMENT, WS}")
+       "{'ATTRIBUTE', 'VERSION', LINE_CONTINUATION, NEWLINE, " +
+       "REMCOMMENT, COMMENT, WS}")
 anti_patterns = [
     [
         '''\
