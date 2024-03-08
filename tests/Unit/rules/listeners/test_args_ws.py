48e4b18dd1573eb6581a01347d2f1325c2092902
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-03-08 10:54:06 -0500

    Update test_args_ws.py

--- tests/Unit/rules/listeners/test_args_ws.py
+++ tests/Unit/rules/listeners/test_args_ws.py
@@ -4,10 +4,30 @@ from vba_linter.rules.rule_base import RuleBase
 from vba_linter.rules.listeners.arglist_ws import ArglistWs
 
 
+msg = "Excess whitespace before '('"
+
+
 anti_patterns = [
     [
         RuleTestBase.worst_practice,
-        [(2, 51, '121', "Excess whitespace before '('")]
+        [(2, 51, '121', msg)]
+    ],
+    [
+        '''\
+Attribute VB_Name = "Foo"
+Public Sub Foo (num)
+    Call Foo (3)
+    Foo (1 + 2)
+    bar = Foo (1 + 2)
+End Sub
+
+Public Property Let Bar (bar)
+End Property
+''',  # noqa
+        [(2, 15, "121", msg),
+         (3, 13, "121", msg),
+         (5, 14, "121", msg),
+         (8, 24, "121", msg)]
     ]
 ]
 
