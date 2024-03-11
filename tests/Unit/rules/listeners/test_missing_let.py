75b46b549f40da29ce34d3211c8f1c7762e5c4d2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-03-11 09:34:08 -0400

    Update test_missing_let.py

--- tests/Unit/rules/listeners/test_missing_let.py
+++ tests/Unit/rules/listeners/test_missing_let.py
@@ -1,5 +1,6 @@
 import pytest
-from antlr4 import TerminalNodeImpl, Token
+from antlr4 import Token
+from antlr4.tree import TerminalNodeImpl
 from antlr4_vba.vbaLexer import vbaLexer
 from antlr4_vba.vbaParser import vbaParser
 from Unit.rules.rule_test_base import RuleTestBase
