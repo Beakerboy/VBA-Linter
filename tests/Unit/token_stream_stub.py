b2df970b3e10a9d5e7f860f6fb8d8e49ddcd7e63
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-03-11 06:16:49 -0400

    Update token_stream_stub.py

--- tests/Unit/token_stream_stub.py
+++ tests/Unit/token_stream_stub.py
@@ -5,5 +5,7 @@ from typing import List, TypeVar
 T = TypeVar('T', bound='CommonTokenStream')
 
 
-def __init__(self: T) -> None
-    ...
+class TokenStreamStub(CommonTokenStream):
+    
+    def __init__(self: T) -> None
+        ...
