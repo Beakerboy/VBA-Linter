94c2ee7a83ce41ce4e88c8486b38730df332301f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-03-11 14:40:59 -0400

    Update string_format.py

--- src/vba_linter/helpers/string_format.py
+++ src/vba_linter/helpers/string_format.py
@@ -3,7 +3,7 @@ import re
 
 def is_snake_case(name: str) -> bool:
     pattern = '(^[a-z]{1}$)|([a-z]+(_[a-z]+)*$)'
-    return cls.text_matches(pattern, name)
+    return text_matches(pattern, name)
 
 
 def is_camel_case(name: str) -> bool:
@@ -11,7 +11,7 @@ def is_camel_case(name: str) -> bool:
     Also known as lowerCamelCase.
     """
     pattern = '(^[a-z]{1}$)|([a-z]{2,}([a-zA-Z]([a-z])+)*$)'
-    return cls.text_matches(pattern, name)
+    return text_matches(pattern, name)
 
 
 def is_pascal_case(name: str) -> bool:
@@ -19,7 +19,7 @@ def is_pascal_case(name: str) -> bool:
     Also known as UpperCamelCase.
     """
     pattern = '(^[a-z]{1}$)|(([A-Z]([a-z])+)*$)'
-    return cls.text_matches(pattern, name)
+    return text_matches(pattern, name)
 
 
 def text_matches(pattern: str, name: str) -> bool:
