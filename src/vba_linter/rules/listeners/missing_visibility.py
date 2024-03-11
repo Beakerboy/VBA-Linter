be1efe662e2dfca70ee75799bf2e71012aba7d39
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-03-11 14:49:20 -0400

    Update missing_visibility.py

--- src/vba_linter/rules/listeners/missing_visibility.py
+++ src/vba_linter/rules/listeners/missing_visibility.py
@@ -1,13 +1,13 @@
 from antlr4 import ParserRuleContext
 from antlr4_vba.vbaParser import vbaParser
 from typing import TypeVar
-from vba_linter.antlr.vbaListener import VbaListener
+from vba_linter.rules.listeners.listener_rule_base import ListenerRuleBase
 
 
 T = TypeVar('T', bound='MissingVisibility')
 
 
-class MissingVisibility(VbaListener):
+class MissingVisibility(ListenerRuleBase):
     def __init__(self: T) -> None:
         super().__init__()
         self.output: list = []
