4495d7974ea667af1c05a6101cc9d67185f951d6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-03-12 13:35:44 -0400

    Declare statement whitespace is ok

--- tests/Unit/rules/rule_test_base.py
+++ tests/Unit/rules/rule_test_base.py
@@ -17,6 +17,7 @@ class RuleTestBase:
         [
             ('Attribute VB_Name = "SQLFactory"\r\n' +
              'Option Explicit\r\n' +
+             'Private Declare Function F Lib "B" (baz)\r\n' +
              '\r\n' +
              'Public Function Foo(ByRef num As String, _\r\n' +
              'bar)\r\n' +
