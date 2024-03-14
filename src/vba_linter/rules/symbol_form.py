8aa9b20ad88c0f826abcd4341f03301fcfffbdd5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-03-14 12:52:12 -0400

    Create symbol_form.py

---
+++ src/vba_linter/rules/symbol_form.py
@@ -0,0 +1,29 @@
+from antlr4 import CommonTokenStream
+from antlr4_vba.vbaLexer import vbaLexer
+from vba_linter.rules.rule_base import RuleBase
+from typing import List, TypeVar
+
+
+T = TypeVar('T', bound='TokenText')
+
+
+class TokenText(RuleBase):
+    """
+    report if the text of a token does not match the target.
+    """
+    def __init__(self: T) -> None:
+        super().__init__()
+        self.rule_name = '183'
+
+    def test(self: T, ts: CommonTokenStream) -> list:
+        output = []
+        targets = {
+            Lexer.GT: ">=", Lexer.LT: "<=", Lexer.NEQ: "<>",
+        }
+        tok = LT(1)
+        if tok.type in targets and tok.text != targets[tok.type]:
+            line = tok.line
+            col = tok.column + 1
+            rule = self.rule_name
+            output.append((line, col, rule))
+        return output
