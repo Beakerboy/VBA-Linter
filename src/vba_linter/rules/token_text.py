74527491ed2bcb60a062022ab03787c9e1d41dab
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-03-15 07:36:17 -0400

    Update token_text.py

--- src/vba_linter/rules/token_text.py
+++ src/vba_linter/rules/token_text.py
@@ -1,7 +1,7 @@
 from antlr4 import CommonTokenStream, Token
 from antlr4_vba.vbaLexer import vbaLexer as Lexer
 from vba_linter.rules.rule_base import RuleBase
-from typing import TypeVar
+from typing import List, TypeVar
 
 
 T = TypeVar('T', bound='TokenText')
@@ -17,7 +17,7 @@ class TokenText(RuleBase):
         self._message = "Incorrect comparison symbol format"
 
     def test(self: T, ts: CommonTokenStream) -> list:
-        output = []
+        output: List[tuple] = []
         targets = {
             Lexer.GT: ">=", Lexer.LT: "<=", Lexer.NEQ: "<>",
         }
