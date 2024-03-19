271e8acea40b5bbb783e7551909b07794cb5236f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-03-19 11:51:44 -0400

    Update test_indent_contains.py

--- tests/Unit/rules/test_indent_contains.py
+++ tests/Unit/rules/test_indent_contains.py
@@ -11,13 +11,13 @@ Public Function Foo(num)
 \tBar = 2
 End Function
 ''',  # noqa
-        [(2, 1, "191")]
+        [(2, 1, "100:200")]
     ],
 ]
 
 
 message_data = [
-    [(2, 1, "191"), ":2:1: W191 indentation contains tabs"],
+    [(2, 1, "100:200"), ":2:1: E200 Indentation contains tabs"],
  ]
 
 
